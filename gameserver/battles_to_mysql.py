5c5b3853843e96dc2155568a8e43531e5b7a915b
Dan Maas <dmaas@spinpunch.com>
2015-01-17 18:18:27 -0800

    battles_to_mysql.py: add min and 10th-percentile frame rates to battles_daily_summary table

--- gameserver/battles_to_mysql.py
+++ gameserver/battles_to_mysql.py
@@ -133,7 +133,9 @@ def battles_summary_schema(sql_util): return {
                ('base_type', 'VARCHAR(8)'),
                ('base_template', 'VARCHAR(32)'),
                ('duration', 'INT8'),
-               ('framerate', 'FLOAT4'),
+               ('framerate', 'FLOAT4'), # average
+               ('framerate_min', 'FLOAT4'), # min
+               ('framerate_10th', 'FLOAT4'), # 10th percentile
                ('victory_ratio', 'FLOAT4'),
                ('loot:xp', 'INT8'),
                ('loot:iron', 'INT8'),
@@ -267,6 +269,9 @@ if __name__ == '__main__':
 
     nosql_client = SpinNoSQL.NoSQLClient(SpinConfig.get_mongodb_config(game_id))
     cur = con.cursor(MySQLdb.cursors.DictCursor)
+
+    # prepare for big GROUP_CONCAT()s below (MySQL-specific)
+    cur.execute("SET @@session.group_concat_max_len = @@global.max_allowed_packet")
 
     # recreate AI bases and analytics tag tables
     if verbose: print 'updating AI bases and analytics tag tables'
@@ -585,7 +590,15 @@ if __name__ == '__main__':
                         "       battles.base_type AS base_type," + \
                         "       battles.base_template AS base_template," + \
                         "       SUM(duration) AS duration," + \
-                        "       AVG(framerate) AS framerate," + \
+                        "       AVG(framerate) AS framerate, " + \
+                        "       MIN(framerate) AS framerate_min, " + \
+                        """     -- MySQL percentile trick
+                                SUBSTRING_INDEX(
+                                  SUBSTRING_INDEX(
+                                    GROUP_CONCAT(framerate ORDER BY framerate SEPARATOR ',')
+                                    , ','
+                                    ,  10/100 * COUNT(*) + 1
+                                    ), ',' , -1) + 0.0 AS framerate_10th, """ + \
                         "       SUM(IF(active_player_outcome='victory',1,0))/COUNT(1) AS victory_ratio," + \
                         "       "+",".join(['SUM('+sql_util.sym('loot:'+res)+')' for res in ('xp','iron','water','res3','iron_lost','water_lost','res3_lost')]) + \
                         "FROM "+sql_util.sym(battles_table)+" battles " + \
