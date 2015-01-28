eda4e489dce58ae8d4b67f40c20036f55c9beea1
Dan Maas <dmaas@spinpunch.com>
2015-01-27 18:11:29 -0800

    staging-server.sh: update to latest version

--- aws/gamemaster/etc/cron.spinpunch.hourly/staging-servers.sh
+++ aws/gamemaster/etc/cron.spinpunch.hourly/staging-servers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 declare -A test_regions
 test_regions[ransomerrift]=test256
@@ -6,15 +6,17 @@ test_regions[thudrunner]=sector201
 test_regions[piratewarmers]=kasei101
 test_regions[flavorysoda]=sector200
 test_regions[tablettransform]=sector200
-test_regions[rummagestones]=sector200
+# test_regions[rummagestones]=sector200 # no regions
 export PYTHONPATH="/home/ec2-user/twisted-13.2.0/lib64/python:$PYTHONPATH"
 
-for GAMEDIR in ransomerrift thudrunner piratewarmers flavorysoda tablettransform rummagestones; do
+# flavorysoda is off now
+for GAMEDIR in ransomerrift thudrunner piratewarmers tablettransform rummagestones; do
         # update to latest code
-        (cd /home/ec2-user/$GAMEDIR/gameserver && svn up .. --quiet && svn cleanup .. && ./stopserver.sh > /dev/null && ./runserver.sh > /dev/null )
+        (cd /home/ec2-user/$GAMEDIR/gameserver && ../scmtool.sh force-up && ./stopserver.sh > /dev/null && rm -f *.pid && ./make-compiled-client.sh && ./runserver.sh > /dev/null )
         # global database maintenance
         (cd /home/ec2-user/$GAMEDIR/gameserver && ./SpinNoSQL.py --maint > /dev/null )
         # region maintenance
-        (cd /home/ec2-user/$GAMEDIR/gameserver && ./maptool.py ${test_regions[$GAMEDIR]} ALL maint --quiet > /dev/null )
+	if [ ${test_regions[$GAMEDIR]} ]; then
+	        (cd /home/ec2-user/$GAMEDIR/gameserver && ./maptool.py ${test_regions[$GAMEDIR]} ALL maint --quiet > /dev/null )
+	fi
 done
-
