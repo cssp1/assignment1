a022893b3ab5b81a1090ad0e1293d859571c9184
Dan Maas <dmaas@spinpunch.com>
2015-02-01 15:08:22 -0800

    gamedata: incorporate verify.py into the Makefile-based build. Now the entire game data build process takes place within one Make invocation, meaning Make can intelligently skip all steps when no input files were updated.

--- gameserver/make-gamedata.sh
+++ gameserver/make-gamedata.sh
@@ -4,7 +4,10 @@
 # Use of this source code is governed by an MIT-style license that can be
 # found in the LICENSE file.
 
-# gamedata build script
+# Gamedata build script
+
+# This really just sets some environment variables and then invokes make on gamedata/$GAME_ID/Makefile
+# (which includes gamedata/Makefile.common to do most of the work).
 
 DO_SNAPSHOT=0
 DO_COMPARE=0
@@ -55,55 +58,13 @@ fi
 
 PROCESSOR_ARGS+=" --game-id $GAME_ID"
 
-echo "Compiling gamedata-${GAME_ID}.json..."
 export SPIN_GAMESERVER="${PWD}" # for SpinConfig to work correctly when invoked outside of gameserver/ dir
 export SPIN_GAMEDATA="${PWD}/../gamedata" # for gamedata Makefiles to work correctly when invoked outside of gameserver/ dir
 export SPIN_GAMECLIENT="${PWD}/../gameclient" # for gamedata Makefiles to work correctly when invoked outside of gameserver/ dir
 export PYTHONPATH="${SPIN_GAMESERVER}:${SPIN_GAMEDATA}:${PYTHONPATH}" # so that gamedata/*.py can pick up gameserver libraries
 export GAME_ID
 
-if [ ! -e ${SPIN_GAMEDATA}/$GAME_ID/built/deps ]; then
-    echo "    Calculating JSON file dependencies..."
-    (cd $SPIN_GAMEDATA && make -f $GAME_ID/Makefile PROCESSOR_ARGS="$PROCESSOR_ARGS" VERIFY_ARGS="$VERIFY_ARGS" dep)
-fi
-
-(cd $SPIN_GAMEDATA && make -f $GAME_ID/Makefile PROCESSOR_ARGS="$PROCESSOR_ARGS" VERIFY_ARGS="$VERIFY_ARGS" -j all)
-if [[ $? != 0 ]]; then
-    echo "Error in gamedata source files! Aborting build."
-    (cd $SPIN_GAMEDATA && rm -f "${GAME_ID}/built/gamedata-${GAME_ID}.json.unverified")
-    exit 1
-fi
-
-if [[ $DO_VERIFY == 1 ]]; then
-    echo "Checking gamedata-${GAME_ID}.json for errors..."
-    (cd $SPIN_GAMEDATA && ./verify.py $VERIFY_ARGS \
-        "$GAME_ID/built/gamedata-${GAME_ID}.json.unverified" \
-        "$GAME_ID/built/${GAME_ID}_ai_bases_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_ai_attacks_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_quarries_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_hives_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_loot_tables_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_promo_codes_compiled.json" \
-        "$GAME_ID/built/${GAME_ID}_server_compiled.json" \
-    "$GAME_ID/built/${GAME_ID}_loading_screens_compiled.json"
-    )
-    if [[ $? != 0 ]]; then
-        echo "Error in gamedata!"
-        (cd $SPIN_GAMEDATA && rm -f "${GAME_ID}/built/gamedata-${GAME_ID}.json.unverified")
-        exit 1
-    fi
-fi
-
-(cd $SPIN_GAMEDATA && cp "${GAME_ID}/built/gamedata-${GAME_ID}.json.unverified" "${GAME_ID}/built/gamedata-${GAME_ID}.json")
-
-# NOTE! gamedata.js is no longer exposed to players, instead one of the -locale.js files must be used!
-# we generate gamedata.js from gamedata.json ONLY for debugging with ?locale_override=null
-# (cd ${SPIN_GAMEDATA} && echo -n "var gamedata = " > "$GAME_ID/built/gamedata-${GAME_ID}.js" && cat "$GAME_ID/built/gamedata-${GAME_ID}.json" >> "$GAME_ID/built/gamedata-${GAME_ID}.js")
-
-# now we have built/gamedata-mf.json
-# create built/gamedata-mf-en_US.json (localization) and .js (prepending "var gamedata = " and linebreaking)
-
-# linebreak tool, relative to gamedata directory
+# find linebreak tool, relative to gamedata directory
 if [ -x "${SPIN_GAMEDATA}/linebreak/built/linebreak" ]; then
     LINEBREAK="./linebreak/built/linebreak"
 elif (cd "${SPIN_GAMEDATA}/linebreak" && make); then
@@ -115,19 +76,34 @@ else
 fi
 export LINEBREAK
 
+# get list of localizations to create
 MYLOC_LIST=`find ${SPIN_GAMEDATA}/${GAME_ID}/localize -name '*.po' | xargs basename -a -s .po | sed "s/^${GAME_ID}-//" | sort`
-
 MYTARGETS=""
 for MYLOC in $MYLOC_LIST; do
     MYTARGETS+=" ${GAME_ID}/built/gamedata-${GAME_ID}-${MYLOC}.js"
 done
-(cd "${SPIN_GAMEDATA}" && make -f "${GAME_ID}/Makefile" -j $MYTARGETS)
+
+echo "Compiling gamedata-${GAME_ID}.json..."
+
+# update make dependency info, if deps file is missing
+if [ ! -e ${SPIN_GAMEDATA}/$GAME_ID/built/deps ]; then
+    echo "    Calculating JSON file dependencies..."
+    (cd $SPIN_GAMEDATA && make -f $GAME_ID/Makefile PROCESSOR_ARGS="$PROCESSOR_ARGS" VERIFY_ARGS="$VERIFY_ARGS" dep)
+fi
+
+# do everything in one make invocation!
+(cd $SPIN_GAMEDATA && make -f $GAME_ID/Makefile PROCESSOR_ARGS="$PROCESSOR_ARGS" DO_VERIFY="$DO_VERIFY" VERIFY_ARGS="$VERIFY_ARGS" -j all $MYTARGETS)
 if [[ $? != 0 ]]; then
-    echo "Error creating localization!"
+    echo "Error in gamedata! Aborting build."
     exit 1
 fi
 
-# snapshot comparison should be based on a localized gamedata
+# NOTE! gamedata.js is no longer exposed to players, instead one of the -locale.js files must be used!
+# we generate gamedata.js from gamedata.json ONLY for debugging with ?locale_override=null
+# (cd ${SPIN_GAMEDATA} && echo -n "var gamedata = " > "$GAME_ID/built/gamedata-${GAME_ID}.js" && cat "$GAME_ID/built/gamedata-${GAME_ID}.json" >> "$GAME_ID/built/gamedata-${GAME_ID}.js")
+
+# for regression testing:
+# make a snapshot comparison based on localized gamedata
 SNAPSRC="$GAME_ID/built/gamedata-${GAME_ID}-en_US.json"
 SNAPREF="$GAME_ID/built/gamedata-${GAME_ID}-en_US.json.good"
 if [[ $DO_SNAPSHOT == 1 ]]; then
