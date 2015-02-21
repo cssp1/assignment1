20c22ad4b742684b9cf9705ebb96b82c6913662c
Dan Maas <dmaas@spinpunch.com>
2015-02-21 10:05:24 -0800

    analytics1: automate backups of SQL upcache analytics databases

---
+++ aws/analytics1/etc/cron.spinpunch.mysql-daily/90-backup-mysql-mf2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+exit 0 # XXX disabled by default
+
+GAME_DIR=/home/ec2-user/marsfrontier2
+DB=mf2_upcache
+SAVE_DIR=/media/ephemeral0a/backup-scratch
+S3_PATH="spinpunch-backups/analytics"
+
+S3_KEYFILE=${HOME}/.ssh/`echo $HOSTNAME | cut -d. -f1`-awssecret
+TARFILE=`date +%Y%m%d`-${DB}.mysql.gz
+ERROR=0
+
+cd $GAME_DIR/gameserver
+
+./mysql.py ${DB} --dump "${SAVE_DIR}/${TARFILE}"
+if [[ $? != 0 ]]; then
+    echo "SQL dump error"
+    exit $?
+fi
+
+$GAME_DIR/aws/aws --secrets-file=${S3_KEYFILE} --md5 put "${S3_PATH}/${TARFILE}" "${SAVE_DIR}/${TARFILE}"
+if [[ $? != 0 ]]; then
+    echo "S3 upload error!"
+    ERROR=1
+fi
+
+rm -f "${SAVE_DIR}/${TARFILE}"
+
+exit $ERROR
