82c054bc659339842501b0f8afb6e9cf4d156887
Dan Maas <dmaas@spinpunch.com>
2015-06-14 21:24:06 +0800

    SpinETL: update_summary() can how handle arbitrary iterators like month_iterator

--- gameserver/SpinETL.py
+++ gameserver/SpinETL.py
@@ -22,6 +22,21 @@ MAX_SESSION_LENGTH = 43200 # maximum conceivable session length, used to limit q
 def uniform_iterator(start, end, dt):
     return ((x,x+dt) for x in xrange(dt*(start//dt), dt*(end//dt + 1), dt))
 
+# return pairs of (t0,t1) intervals for entire calendar months in the UNIX time range start->end, inclusive
+def month_iterator(start, end, unused_dt):
+    sy,sm,sd = SpinConfig.unix_to_cal(start) # starting year,month,day
+    ey,em,ed = SpinConfig.unix_to_cal(end) # ending year,month,day
+    while sy <= ey and ((sy < ey) or (sm <= em)):
+        lasty = sy
+        lastm = sm
+        # compute next month
+        sm += 1
+        if sm > 12:
+            sm = 1
+            sy += 1
+
+        yield (SpinConfig.cal_to_unix((lasty,lastm,1)), SpinConfig.cal_to_unix((sy,sm,1)))
+
 # iterator for a MongoDB log_* table
 def iterate_from_mongodb(game_id, table_name, start_time, end_time, query = None):
     nosql_client = SpinNoSQL.NoSQLClient(SpinConfig.get_mongodb_config(game_id))
@@ -105,9 +120,10 @@ def get_denormalized_summary_props(gamedata, player, user, format):
 # affected: a set of the start times of intervals that have changed since the last update
 # source_range: the time range covered by the complete set of input data events
 # interval: a string name for the interval column ("day", "hour", etc)
-# dt: the time offset between successive intervals, in seconds
+# dt: the time offset between successive intervals, in seconds. Use 0 when using month_iterator
 # origin: "zero point" of time intervals (should be 0 except for weekly summaries that use gamedata['matchmaking']['week_origin'])
 # execute_func: called to actually fill in the summary data
+# iterator: time iteration function. Can be uniform_iterator or month_iterator.
 # resummarize_tail: Unconditionally resummarize final entries within this many seconds of the "tail" of the existing summary,
 #                   even if it they are not mentioned in the "affected" set.
 #  * note: by default this function assumes that any existing summary data is fully valid
@@ -116,7 +132,7 @@ def get_denormalized_summary_props(gamedata, player, user, format):
 #  then "resummarize_tail" must be set appropriately
 
 def update_summary(sql_util, con, cur, table, affected, source_range, interval, dt,
-                   origin = 0, verbose = True, dry_run = False, execute_func = None,
+                   origin = 0, verbose = True, dry_run = False, execute_func = None, iterator = uniform_iterator,
                    resummarize_tail = 0):
 
     # check how much summary data we already have
@@ -124,22 +140,28 @@ def update_summary(sql_util, con, cur, table, affected, source_range, interval, 
     rows = cur.fetchall()
     if rows and rows[0] and rows[0]['begin'] and rows[0]['end']:
         # we already have summary data - update it incrementally
+
+        # convert "affected" from a list of start times to a list of start,end times for each interval
+        affected = set(map(lambda xy: (xy[0]+origin,xy[1]+origin), (next(iterator(x, x+dt, dt)) for x in affected)))
+
         if source_range: # fill in any missing trailing summary data
-            source_days = sorted(affected.union(set(xrange(dt*((rows[0]['end']-origin + dt - resummarize_tail)//dt)+origin,
-                                                           dt*((source_range[1]-origin)//dt + 1)+origin, dt))))
+            # skip dt past final row, unless we want to resummarize the last interval(s)
+            source_days = sorted(affected.union(map(lambda xy: (xy[0]+origin, xy[1]+origin), iterator(rows[0]['end'] - origin + dt - resummarize_tail, source_range[1] - origin, dt))))
         else:
             source_days = sorted(list(affected))
     else:
         # recreate entire summary
         if source_range:
-            source_days = range(dt*((source_range[0]-origin)//dt)+origin, dt*((source_range[1]-origin)//dt + 1)+origin, dt)
+            source_days = map(lambda xy: (xy[0]+origin, xy[1]+origin), iterator(source_range[0]-origin, source_range[1]-origin, dt))
         else:
             source_days = None
 
     if source_days:
-        for day_start in source_days:
-            if verbose: print 'updating', table, 'at', day_start, time.strftime('%Y%m%d', time.gmtime(day_start))
-
+        for day_start, day_end in source_days:
+            dt = day_end - day_start
+            if verbose:
+                time_fmt = '%Y%m%d %H:%M:%S'
+                print 'updating', table, 'at', day_start, time.strftime(time_fmt, time.gmtime(day_start)), '-', time.strftime(time_fmt, time.gmtime(day_end)), 'dt', dt
             if (not dry_run):
                 # delete entries for the date range we're about to update
                 cur.execute("DELETE FROM "+sql_util.sym(table)+" WHERE "+sql_util.sym(interval)+" >= %s AND "+sql_util.sym(interval)+" < %s+%s", [day_start,day_start,dt])
