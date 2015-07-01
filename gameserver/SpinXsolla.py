0fd8be3c3f36a012ed10f8984afad195a57d0dea
Dan Maas <dmaas@spinpunch.com>
2015-07-01 13:38:30 +0900

    Xsolla: set up currency slates, removing redundant SKUs and flagging ones that will cause problems with Xsolla's (currency,price) lookup

--- gameserver/SpinXsolla.py
+++ gameserver/SpinXsolla.py
@@ -45,22 +45,74 @@ def make_virtual_currency_settings_update(config, gamedata):
                   'type': 'packets', 'allow_user_sum': False, # do not allow users to change the purchase amount
                   'packets': {}
                   }
+
     # now the currency slates
+
+    # currency/price/quantity triplets are redundant from Xsolla's point of view, so only emit one
+    by_currency_and_price_and_quantity = {}
+
+    # Xsolla looks up SKUs by currency and price, so we cannot have
+    # SKUs at the same currency and price with different Gamebucks quantities!
+    by_currency_and_price = {}
+
+    # skip SKUs where the "requires" predicate has an A/B test in it
+    def skip_predicate(pred, depth = 0):
+        if pred['predicate'] == 'AND':
+            return any(skip_predicate(p, depth=depth+1) for p in pred['subpredicates'])
+        elif pred['predicate'] == 'OR':
+            return any(skip_predicate(p, depth=depth+1) for p in pred['subpredicates'])
+        elif pred['predicate'] == 'NOT':
+            return not skip_predicate(pred['subpredicates'][0], depth=depth+1)
+        elif pred['predicate'] == 'ANY_ABTEST' and not pred['key'].endswith('_override'):
+            return True
+        elif pred['predicate'] == 'ALWAYS_FALSE':
+            return True
+        return False
+
     for spellname, data in gamedata['spells'].iteritems():
-        if 'open_graph_prices' in data and data.get('currency','').startswith('fbpayments:'):
+        if data.get('currency','').startswith('xsolla:'):
             currency = data['currency'].split(':')[1]
+            if currency == '*': continue # arbitrary
+
+            # it's really hard to figure out which SKUs to include just by looking at the predicates,
+            # so use a hacky method for now...
+            if 'D1_' in spellname or 'D3_' in spellname or \
+               'SALE_' in spellname or \
+               'P050XY' in spellname:
+                # turn off this SKU
+                continue
+
+            if 0 and 'requires' in data:
+                if skip_predicate(data['requires']):
+                    continue
+
             if currency not in body_json['packets']:
                 body_json['packets'][currency] = []
+
+            key3 = (currency, data['price'], data['quantity'])
+            if key3 in by_currency_and_price_and_quantity: continue # currency/price/quantity triplet is redundant - skip this
+
             packet = {'amount': data['quantity'],
                       'price': data['price'],
                       'image_url': '//s3.amazonaws.com/'+config['public_s3_bucket']+'/facebook_assets/'+gamedata['store']['fb_open_graph_gamebucks_icon'],
                       'description': { 'en': data['ui_name'].replace('%GAMEBUCKS_QUANTITY', '%d' % data['quantity']).replace('%GAME_NAME', gamedata['strings']['game_name']).replace('%GAMEBUCKS_NAME', gamedata['store']['gamebucks_ui_name']) },
                       }
+            by_currency_and_price_and_quantity[key3] = packet
+
             body_json['packets'][currency].append(packet)
+
+            # keep track of any overlap in currency/price point, since Xsolla can't discriminate these
+            key2 = (currency, data['price'])
+            if key2 in by_currency_and_price:
+                other_spellname, other_quantity = by_currency_and_price[key2]
+                raise Exception('Xsolla SKU overlap! %s %s %r %d with %s %r %d from %s\n%r\n\n' % (spellname, currency, data['price'], data['quantity'], currency, data['price'], other_quantity, other_spellname, data['requires']))
+            else:
+                by_currency_and_price[key2] = spellname, data['quantity']
+
     return 'https://api.xsolla.com/merchant/projects/%s/virtual_currency' % config['xsolla_project_id'], 'PUT', make_headers(config), SpinJSON.dumps(body_json) # this must be an HTTP "PUT" to work
 
 
-# return the (url, method,headers,body) for a request to generate an Xsolla "token" to start the purchase flow
+# return the (url,method,headers,body) for a request to generate an Xsolla "token" to start the purchase flow
 # @param config - from SpinConfig config.json
 def make_token_request(config,
                        user_xs_id, user_email, user_currency, user_country, user_language,
@@ -94,8 +146,29 @@ def make_token_request(config,
 
 if __name__ == '__main__':
     import SpinConfig
-    if 0:
+    import getopt, sys
+
+    mode = 'test'
+    dry_run = False
+    game_id = SpinConfig.game()
+
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'g:', ['dry-run','update-slates'])
+
+    for key, val in opts:
+        if key == '-g': game_id = val
+        elif key == '--dry-run': dry_run = True
+        elif key == '--update-slates': mode = 'update-slates'
+
+    if mode == 'test':
         print make_token_request(SpinConfig.config, 'xs1234', 'example@example.com',
                                  'USD', 'us', 'en', 100, '100 Gamebucks', 25, int(time.time()) - 10*86400)
-    gamedata = SpinJSON.load(open(SpinConfig.gamedata_filename()))
-    print make_virtual_currency_settings_update(SpinConfig.config, gamedata)[3]
+
+    elif mode == 'update-slates':
+        import requests
+        gamedata = SpinJSON.load(open(SpinConfig.gamedata_filename(override_game_id = game_id)))
+        url, method, headers, body = make_virtual_currency_settings_update(SpinConfig.config, gamedata)
+        if dry_run:
+            print 'update:', SpinJSON.dumps(SpinJSON.loads(body), pretty=True)
+        else:
+            req = getattr(requests, method.lower())(url, headers=headers, data=body)
+            print req.status_code, req.text
