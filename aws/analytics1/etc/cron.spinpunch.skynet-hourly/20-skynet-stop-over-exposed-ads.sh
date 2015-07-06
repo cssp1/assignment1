85050b8e0be889161b15c56ba882538eed96d27b
Dan Maas <dmaas@spinpunch.com>
2015-07-06 16:18:08 +0900

    stop u4 ads at frequency 5 instead of 10 to cut them off earlier

--- aws/analytics1/etc/cron.spinpunch.skynet-hourly/20-skynet-stop-over-exposed-ads.sh
+++ aws/analytics1/etc/cron.spinpunch.skynet-hourly/20-skynet-stop-over-exposed-ads.sh
@@ -4,7 +4,7 @@ GAME_DIR=/home/ec2-user/thunderrun
 cd ${GAME_DIR}/gameserver
 
 (for KIND in bx br bs; do \
-  ./skynet.py --mode adgroups-delete --filter ${KIND}_u4 --max-freq 10 --min-age 14400 > /dev/null && \
+  ./skynet.py --mode adgroups-delete --filter ${KIND}_u4 --max-freq 5 --min-age 14400 > /dev/null && \
   ./skynet.py --mode adgroups-delete --filter ${KIND}_u32 --max-frequency 2 --min-age 14400 > /dev/null; \
 done) && \
 ./skynet.py --mode adcampaigns-collect-garbage > /dev/null
