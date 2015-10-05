274ae731594db06740e24334840d9edacb9075b6
Dan Maas <dmaas@spinpunch.com>
2015-10-06 02:14:15 +0900

    tournament-winners script now uploads list to Dropbox and notifies Mattermost

--- gameserver/tournament-winners.sh
+++ gameserver/tournament-winners.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# query tournament winners, send prizes, record results to file, and SNS broadcast result.
+# query tournament winners, send prizes, record results to file, and upload/broadcast results.
 
 . /etc/spinpunch
 
@@ -14,11 +14,52 @@ SEASON=$2
 WEEK=$3
 TOURNAMENT_STAT=$4
 
+DROPBOX_ACCESS_TOKEN=`cat ${HOME}/.ssh/dropbox-access-token`
+GAME_ID_UPPER=`echo ${GAME_ID} | tr a-z A-Z`
+
+declare -A PLATFORM_UI_NAMES
+PLATFORM_UI_NAMES[fb]=Facebook
+PLATFORM_UI_NAMES[ag]=ArmorGames
+PLATFORM_UI_NAMES[kg]=Kongregate
+
 sleep `python -c "import time; print int(${END_TIME}-time.time());"`
 for CONT in $GAME_TOURNAMENT_CONTINENTS; do
+    NUMERIC_DATE=`date +%Y%m%d`
     LOGFILE="/var/tmp/`date +%s`-winners-s${SEASON}-wk${WEEK}-${CONT}.txt"
+    UI_PLATFORM="${PLATFORM_UI_NAMES[$CONT]}"
+
+    # get winner list and award prizes
     ./SpinNoSQL.py --winners --season ${SEASON} --week ${WEEK} --tournament-stat ${TOURNAMENT_STAT} --score-scope continent --score-loc $CONT --send-prizes > $LOGFILE
-    /usr/bin/aws sns publish --topic-arn ${GAME_TOURNAMENT_WINNERS_SNS_TOPIC} --subject "${GAME_ID} ${CONT} Winners" --message "`cat ${LOGFILE}`" > /dev/null
+
+    # upload to dropbox
+    DROPBOX_FILENAME="${NUMERIC_DATE}-${GAME_ID_UPPER}-winners-week${WEEK}-${UI_PLATFORM}.txt"
+    DROPBOX_PATH="/tournament_winners/${DROPBOX_FILENAME}"
+
+    curl -s -X POST https://content.dropboxapi.com/2-beta-2/files/upload \
+	 --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
+	 --header "Content-Type: application/octet-stream" \
+	 --header "Dropbox-API-Arg: {\"path\": \"${DROPBOX_PATH}\",\"mode\": \"overwrite\",\"autorename\": false,\"mute\": false}" \
+	 --data-binary "@${LOGFILE}" > /dev/null
+
+    # get dropbox shared link
+    SHARED_LINK_RESULT=`curl -s -X POST https://api.dropboxapi.com/2-beta-2/sharing/create_shared_link \
+	 --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
+	 --header "Content-Type: application/json" \
+	 --data "{\"path\": \"${DROPBOX_PATH}\",\"short_url\": false}"`
+    SHARED_LINK_URL=`echo $SHARED_LINK_RESULT | python -c 'import json, sys; print json.load(sys.stdin)["url"];'`
+
+    # compile notification
+    NOTIFICATION_SUBJECT="${NUMERIC_DATE} ${GAME_ID_UPPER} Tournament Winners in ${UI_PLATFORM}"
+    NOTIFICATION_MESSAGE_BRIEF="Prizes have been sent. Winner list at: ${SHARED_LINK_URL}"
+    NEWLINE=$'\n'
+    NOTIFICATION_MESSAGE_FULL="${NOTIFICATION_MESSAGE_BRIEF}${NEWLINE}`cat ${LOGFILE}`"
+
+    # send SNS notification
+    /usr/bin/aws sns publish --topic-arn "${GAME_TOURNAMENT_WINNERS_SNS_TOPIC}" --subject "${NOTIFICATION_SUBJECT}" --message "${NOTIFICATION_MESSAGE_FULL}" > /dev/null
+
+    # send SpinReminder notification
+    ./SpinReminders.py --from "tournament-winners.sh" --subject "${NOTIFICATION_SUBJECT}" --body "${NOTIFICATION_MESSAGE_BRIEF}" \
+		       --recipient '{"type":"mattermost", "channel":"marketing"}'
 done
 
 
