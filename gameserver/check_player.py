3ba03a1ca0c34941b2399e9571790591efff9e59
Dan Maas <dmaas@spinpunch.com>
2015-10-12 10:41:24 +0900

    omit unimportant customer_support history from check_player output

--- gameserver/check_player.py
+++ gameserver/check_player.py
@@ -527,6 +527,7 @@ if __name__ == '__main__':
         if 'customer_support' in player['history']:
             print fmt % ('Customer Support history', '')
             for entry in player['history']['customer_support']:
+                if entry['method'] in ('record_alt_login', 'reset_idle_check_state'): continue # don't bother printing these
                 print '    At %s by %s: %s %s' % (time.strftime('%Y%m%d %H:%M GMT', time.gmtime(entry['time'])), entry['spin_user'], entry['method'].upper(), SpinJSON.dumps(entry.get('args',{})))
                 print '        Reason: %s' % entry['ui_reason']
 
