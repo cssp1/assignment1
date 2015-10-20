9e33f843bd035abc493eac25382e1fe878a83cff
Dan Maas <dmaas@spinpunch.com>
2015-10-20 16:31:58 +0900

    run SpinConfig.py in the gameserver/ directory

--- aws/daily-metrics.sh
+++ aws/daily-metrics.sh
@@ -46,7 +46,7 @@ fi
 touch $HOME/.awsrc
 
 GAME_NAME=`basename $GAME_DIR`
-GAME_ID=`$GAME_DIR/gameserver/SpinConfig.py --getvar game_id --getvar-format raw`
+GAME_ID=`(cd $GAME_DIR/gameserver && ./SpinConfig.py --getvar game_id --getvar-format raw)`
 GAME_ID_UPPER=`echo ${GAME_ID} | tr [a-z] [A-Z]`
 
 TODAY=`date +%Y%m%d`
@@ -151,7 +151,7 @@ echo "sending SMS message..."
 (cd $GAME_DIR/gameserver && \
     ./SpinReminders.py \
         --body-from "$SMSFILE" --from "$UI_MAIL_SENDER" --subject "${GAME_ID_UPPER} Heartbeat" \
-        --recipients "`${GAME_DIR}/gameserver/SpinConfig.py --getvar heartbeat_recipients`")
+        --recipients "`(cd ${GAME_DIR}/gameserver && ./SpinConfig.py --getvar heartbeat_recipients)`")
 rm -f "$SMSFILE"
 
 exit $ERROR
