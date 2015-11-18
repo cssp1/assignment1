1eaeb94f0a1ac2cf701978d3fc626152132569ff
Dan Maas <dmaas@spinpunch.com>
2015-11-17 19:44:41 -0600

    avoid ascii encoding errors when reporting OGPAPI exceptions

--- gameserver/server.py
+++ gameserver/server.py
@@ -12979,7 +12979,7 @@ class OGPAPI(resource.Resource):
 
     def render_GET(self, request):
         SpinHTTP.set_access_control_headers(request)
-        ret = catch_all('OGPAPI request %r args %r' % (request, request.args))(self.handle_request)(request)
+        ret = catch_all('OGPAPI request %s args %r' % (request.uri.encode('utf-8'), dict((k,v[0].encode('utf-8')) for k,v in request.args.iteritems())))(self.handle_request)(request)
         if ret is None:
             request.setResponseCode(http.BAD_REQUEST)
             ret = 'spinpunch error'
