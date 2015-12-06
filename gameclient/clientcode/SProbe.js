334ed204c6f9366bb47b57bcf86b9defa12411ff
Dan Maas <dmaas@spinpunch.com>
2015-12-06 20:49:29 +0900

    update Copyright info to 2015 Battlehouse Inc.

--- gameclient/clientcode/SProbe.js
+++ gameclient/clientcode/SProbe.js
@@ -1,6 +1,6 @@
 goog.provide('SProbe');
 
-// Copyright (c) 2015 SpinPunch Studios. All rights reserved.
+// Copyright (c) 2015 Battlehouse Inc. All rights reserved.
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
--- gamedata/linebreak/linebreak.c
+++ gamedata/linebreak/linebreak.c
@@ -1,7 +1,7 @@
 /* Break long lines in a JavaScript file, without changing its meaning at all. */
 /* This should work identically to linebreak.py, just a lot faster. */
 
-/* Copyright (c) 2015 SpinPunch Studios. All rights reserved.
+/* Copyright (c) 2015 Battlehouse Inc. All rights reserved.
    Use of this source code is governed by an MIT-style license that can be
    found in the LICENSE file. */
 
