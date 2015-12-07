fb27f787f44d998e79c90749a85c257c08b70d4e
Dan Maas <dmaas@spinpunch.com>
2015-12-07 15:20:58 +0900

    add space-insensitive chat filter, to stop things like "you j e r k"

--- gameclient/clientcode/ChatFilter.js
+++ gameclient/clientcode/ChatFilter.js
@@ -8,16 +8,47 @@ goog.provide('ChatFilter');
     Crude bad-language filter. Same as ChatFilter.py on the server.
 */
 
-/** @type {RegExp|null} */
+/** @type {RegExp|null} main bad-word filter */
 ChatFilter.bad_regex = null;
+
+// to catch bad words that have interspersed whitespace, e.g. "you d o r k",
+// we also operate on a "compressed" version of the string that replaces all
+// whitespace runs with a single "marker" character (which is treated as optional
+// during the main regex match, if the match candidate begins with a marker).
+// Note: it doesn't work to always treat all whitespace as optional, since that
+// would falsely flag "rush it" (whereas "r u a sh it" should be flagged).
+
+/** @type {string|null} space replacement character */
+ChatFilter.space_marker = null;
+/** @type {string|null} regex-quoted version of space_marker */
+ChatFilter.space_marker_pattern = null;
+
+/** @type {RegExp|null} sensitive to all whitespace (but must also include space_marker) */
+ChatFilter.whitespace_regex = null;
+
+/** @type {RegExp|null} space-insensitive bad-word filter */
+ChatFilter.space_bad_regex = null;
 
 /** @param {!Object} config */
 ChatFilter.init = function(config) {
     var pat_ls = [];
     var word_space;
 
+    ChatFilter.space_marker = /** @type {string|null} */ (config['space_marker']);
+    ChatFilter.space_marker_pattern = /** @type {string|null} */ (config['space_marker_pattern']);
+
     // substitute for spacing between letters
-    var leet_speak_space = /** @type {string|null} */ (config['leet_speak']['word_space']);
+    var leet_speak_space = /** @type {string|null} */ (config['leet_speak']['letter_space']);
+
+    // ensure it includes the whitespace_marker_pattern, if one exists
+    if(ChatFilter.space_marker_pattern) {
+        if(!leet_speak_space) {
+            leet_speak_space = ChatFilter.space_marker_pattern;
+        } else if(leet_speak_space.indexOf(ChatFilter.space_marker_pattern) === -1) {
+            leet_speak_space += ChatFilter.space_marker_pattern;
+        }
+    }
+
     if(leet_speak_space) {
         word_space = '[' + leet_speak_space + ']*';
     } else {
@@ -42,16 +73,70 @@ ChatFilter.init = function(config) {
         pat_ls.push(pat);
     }
 
-
+    // construct main filter regex, which looks for bad words with ordinary word boundaries
     var pattern = '\\b('+pat_ls.join('|')+')\\b';
     ChatFilter.bad_regex = new RegExp(pattern, /** @type {string} */ (config['options']));
+
+    if(ChatFilter.space_marker_pattern) {
+        // construct space-insensitive regex, which looks for bad words with interspersed space_markers *that begin and end with a space_marker*
+        var space_pattern = ChatFilter.space_marker_pattern+'('+pat_ls.join('|')+')'+ChatFilter.space_marker_pattern;
+        ChatFilter.space_bad_regex = new RegExp(space_pattern, /** @type {string} */ (config['options']));
+        ChatFilter.whitespace_regex = new RegExp('['+ChatFilter.space_marker_pattern+'\\s'+']+', 'g');
+    }
+};
+
+/** Create a "compressed" version of a string by replacing all whitespace with the whitespace_marker
+    e.g. "Hey you are a j e r k" -> ".Hey.you.are.a.j.e.r.k."
+    We will then search for bad words that begin after a marker.
+    Also returns an array with the index of each character in the original string, for backwards mapping.
+    @param {string} s
+    @return {{compressed: string,
+              index_map: !Array<number>}} */
+ChatFilter.compress_string = function(s) {
+    /** @type {string} */
+    var r = '';
+    /** @type {!Array<number>} */
+    var index_map = [];
+
+    // begin with a space marker
+    r += ChatFilter.space_marker;
+    index_map.push(-1);
+
+    for(var i = 0, run = false; i < s.length; i++) {
+        var c = s.charAt(i);
+        if(c.match(ChatFilter.whitespace_regex)) {
+            if(!run) {
+                run = true;
+                r += ChatFilter.space_marker;
+                index_map.push(-1);
+            } else {
+                // absorb the run
+            }
+        } else {
+            run = false;
+            r += c;
+            index_map.push(i);
+        }
+    }
+
+    // end with a space marker
+    r += ChatFilter.space_marker;
+    index_map.push(-1);
+
+    return { compressed: r, index_map: index_map };
 };
 
 /** Check a string for bad words
     @param {string} s
     @return {boolean} */
 ChatFilter.is_bad = function(s) {
-    return s.search(ChatFilter.bad_regex) != -1;
+    if(s.search(ChatFilter.bad_regex) != -1) { return true; }
+    if(ChatFilter.space_bad_regex) {
+        var compressed = ChatFilter.compress_string(s).compressed;
+        ChatFilter.space_bad_regex.lastIndex = 0; // reset the regex
+        if(compressed.search(ChatFilter.space_bad_regex) != -1) { return true; }
+    }
+    return false;
 };
 
 /** Replace a bad word with asterisks of the same string length
@@ -66,8 +151,40 @@ ChatFilter.censor_replacer = function(rep) {
 };
 
 /** Replace bad words in a string with asterisks
-    @param {string} s
+    @param {string} original_s
     @return {string} */
-ChatFilter.censor = function(s) {
-    return s.replace(ChatFilter.bad_regex, ChatFilter.censor_replacer);
+ChatFilter.censor = function(original_s) {
+    var s = original_s.replace(ChatFilter.bad_regex, ChatFilter.censor_replacer);
+
+    if(ChatFilter.space_bad_regex) {
+        var comp = ChatFilter.compress_string(original_s);
+
+        // back-patch the string with * wherever bad words were detected in the compressed version
+        ChatFilter.space_bad_regex.lastIndex = 0; // reset the regex
+        var match;
+        while ((match = ChatFilter.space_bad_regex.exec(comp.compressed)) !== null) {
+            var index = match.index, length = match[0].length;
+
+            // replace the entire run in the source string with asterixes
+            var begin = -1, end = -1;
+            for(var i = index; i < index+length; i++) {
+                var s_index = comp.index_map[i];
+                if(s_index >= 0) { // accumulate bounds
+                    begin = (begin >= 0 ? Math.min(begin, s_index) : s_index);
+                    end = (end >= 0 ? Math.max(end, s_index) : s_index);
+                }
+            }
+
+            // replace the entire begin-end range (inclusive)
+            if(begin >= 0 && end >= begin) {
+                var new_s = s.substr(0,begin);
+                for(var i = 0; i < end-begin+1; i++) {
+                    new_s += "*";
+                }
+                new_s += s.substr(end+1);
+                s = new_s;
+            }
+        }
+    }
+    return s;
 };
--- gameserver/ChatFilter.py
+++ gameserver/ChatFilter.py
@@ -4,7 +4,8 @@
 # Use of this source code is governed by an MIT-style license that can be
 # found in the LICENSE file.
 
-# crude bad-language filter. Same as ChatFilter.js on the client.
+# crude bad-language filter.
+# Identical to ChatFilter.js on the client (see that file for more detailed comments).
 
 import re
 import collections
@@ -17,9 +18,21 @@ class ChatFilter(object):
 
         pat_ls = []
 
+        self.space_marker = config.get('space_marker',None)
+        self.space_marker_pattern = config.get('space_marker_pattern',None)
+
         # substitute for spacing between letters
-        if config['leet_speak'].get('word_space',None):
-            word_space = '['+config['leet_speak']['word_space']+']*'
+        leet_speak_space = config['leet_speak'].get('letter_space',None)
+
+        # ensure it includes the whitespace_marker_pattern, if one exists
+        if self.space_marker_pattern:
+            if not leet_speak_space:
+                leet_speak_space = self.space_marker_pattern
+            elif self.space_marker_pattern not in leet_speak_space:
+                leet_speak_space += self.space_marker_pattern
+
+        if leet_speak_space:
+            word_space = '[' + leet_speak_space + ']*'
         else:
             word_space = ''
 
@@ -38,10 +51,68 @@ class ChatFilter(object):
         if 'i' in config['options']: flags |= re.I
         # no 'g' option, re is greedy by default
         self.bad_regex = re.compile(pattern, flags)
+
+        if self.space_marker_pattern:
+            space_pattern = self.space_marker_pattern+'('+'|'.join(pat_ls)+')'+self.space_marker_pattern
+            self.space_bad_regex = re.compile(space_pattern, flags)
+            self.whitespace_regex = re.compile('['+self.space_marker_pattern+'\\s'+']+')
+        else:
+            self.space_bad_regex = None
+            self.whitespace_regex = None
+
+    def compress_string(self, s):
+        r = ''
+        index_map = []
+
+        # begin with a space marker
+        r += self.space_marker
+        index_map.append(-1)
+
+        run = False
+        for i in xrange(len(s)):
+            c = s[i]
+            if self.whitespace_regex.match(c):
+                if not run:
+                    run = True
+                    r += self.space_marker
+                    index_map.append(-1)
+            else:
+                run = False
+                r += c
+                index_map.append(i)
+
+        # end with a space marker
+        r += self.space_marker
+        index_map.append(-1)
+
+        return r, index_map
+
     def is_bad(self, input):
-        return bool(self.bad_regex.search(input))
-    def censor(self, input):
-        return self.bad_regex.sub(lambda match: '*'*len(match.group()), input)
+        if self.bad_regex.search(input): return True
+        if self.space_bad_regex:
+            compressed, index_map = self.compress_string(input)
+            if self.space_bad_regex.search(compressed): return True
+        return False
+
+    def censor(self, original_s):
+        s = self.bad_regex.sub(lambda match: '*'*len(match.group()), original_s)
+
+        if self.space_bad_regex:
+            compressed, index_map = self.compress_string(original_s)
+            # back-patch the string with * wherever bad words were detected in the compressed version
+            for match in self.space_bad_regex.finditer(compressed):
+                index = match.start(0); length = match.end(0) - index
+                begin = -1; end = -1
+                for i in xrange(index, index+length):
+                    s_index = index_map[i]
+                    if s_index >= 0: # accumulate bounds
+                        begin = min(begin, s_index) if begin >= 0 else s_index
+                        end = max(end, s_index) if end >= 0 else s_index
+
+                # replace the entire begin-end range (inclusive)
+                if begin >= 0 and end >= begin:
+                    s = s[:begin] + '*'*(end-begin+1) + s[end+1:]
+        return s
 
     # check for abusive chat messages that repeat single letters or digraphs too much
     def is_spammy(self, input, min_length = None):
@@ -85,10 +156,21 @@ if __name__ == '__main__':
         'asdf': 'asdf',
         'sh!t': '****',
         'fu!ckers': '********',
-        'dwarf shortage': 'dwarf shortage'
+        'dwarf shortage': 'dwarf shortage',
+        'do not rush it': 'do not rush it',
+        'u r a sh it': 'u r a *****',
+        'f u c k off': '******* off',
+        'azzhole': '*******',
+        'a z z hole': '***** hole',
+        'a z z h ole': '***** h ole',
         }
     for input, expect in TESTS.iteritems():
-        assert cf.censor(input) == expect
+        censor_result = cf.censor(input)
+        if censor_result != expect:
+            raise Exception('wrong censor result: "%s"->"%s" (expected "%s")' % \
+                            (input, censor_result, expect))
+        assert cf.is_bad(input) == (input != expect)
+
         if cf.is_spammy(input, min_length = 0):
             raise Exception('incorrectly is_spammy: %r' % input)
 
