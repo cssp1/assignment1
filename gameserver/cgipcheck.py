6ead3d2506f39f0ad34b7d9d7d76171c5b01826c
Dan Maas <dmaas@spinpunch.com>
2015-12-15 22:21:49 +0000

    PCHECK needs to use internal_listen_host for operating on proxyserver

--- gameserver/cgipcheck.py
+++ gameserver/cgipcheck.py
@@ -340,7 +340,8 @@ def do_action(path, method, args, spin_token_data, nosql_client):
 
             elif method in ('reconfig','change_state','maint_kick','panic_kick','shutdown'):
                 server_name = args['server']
-                row = nosql_client.server_status_query_one({'_id':server_name}, {'hostname':1, 'game_http_port':1, 'game_ssl_port': 1,
+                row = nosql_client.server_status_query_one({'_id':server_name}, {'hostname':1, 'internal_listen_host': 1,
+                                                                                 'game_http_port':1, 'game_ssl_port': 1,
                                                                                  'external_http_port':1, 'external_ssl_port': 1, 'type':1})
                 if not row:
                     raise Exception('server %s not found' % server_name)
@@ -350,7 +351,7 @@ def do_action(path, method, args, spin_token_data, nosql_client):
                 # tell proxyserver to handle it instead of forwarding
                 if row['type'] == 'proxyserver':
                     control_args['server'] = 'proxyserver'
-                result = do_CONTROLAPI(control_args, host = row['hostname'],
+                result = do_CONTROLAPI(control_args, host = row.get('internal_listen_host', row['hostname']),
                                        http_port = row.get('game_http_port',None) or row.get('external_http_port',None),
                                        ssl_port = row.get('game_ssl_port',None) or row.get('external_ssl_port',None)
                                        )
