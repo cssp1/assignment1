6b2891b2bd65d7cbd06e4f9c55f755ef475ac248
Dan Maas <dmaas@spinpunch.com>
2016-01-24 17:37:00 +0900

    engine updates for unit_repair_furnace aura effects

--- gameclient/clientcode/main.js
+++ gameclient/clientcode/main.js
@@ -25062,19 +25062,19 @@ function invoke_aura_context(inv_dialog, slot_xy, slot, aura, show_dropdown) {
     // DESCRIPTION
     var descr = spec['ui_description' + (('stack' in aura) && (aura['stack'] > 1) && ('ui_description_plural' in spec) ? '_plural' : '')];
     var strength = ('strength' in aura ? aura['strength'] : 1);
-    if(descr.indexOf('%abspct') >= 0) {
+    while(descr.indexOf('%abspct') >= 0) {
         descr = descr.replace('%abspct', ((1.0+strength)*100.0).toFixed(0));
     }
-    if(descr.indexOf('%pct') >= 0) {
+    while(descr.indexOf('%pct') >= 0) {
         descr = descr.replace('%pct', ((strength)*100.0).toFixed(0));
     }
-    if(descr.indexOf('%oneminuspct') >= 0) {
+    while(descr.indexOf('%oneminuspct') >= 0) {
         descr = descr.replace('%oneminuspct', ((1.0-strength)*100.0).toFixed(0));
     }
-    if(descr.indexOf('%price') != -1) {
+    while(descr.indexOf('%price') != -1) {
         descr = descr.replace('%price', Store.display_user_currency_amount(Store.convert_credit_price_to_user_currency(strength), 'full'));
     }
-    if(descr.indexOf('%secteam') != -1) {
+    while(descr.indexOf('%secteam') != -1) {
         var unit_dic = strength[0]['units'];
         var ls = [];
         goog.object.forEach(unit_dic, function(qty, specname) {
@@ -25082,19 +25082,19 @@ function invoke_aura_context(inv_dialog, slot_xy, slot, aura, show_dropdown) {
         });
         descr = descr.replace("%secteam", ls.join(', '));
     }
-    if(descr.indexOf('%stack') != -1) {
+    while(descr.indexOf('%stack') != -1) {
         descr = descr.replace('%stack', pretty_print_number(('stack' in aura ? aura['stack'] : 1)));
     }
     if('extra_ui_description' in aura) {
         descr += aura['extra_ui_description'];
     }
-    if(descr.indexOf('%level') >= 0) {
+    while(descr.indexOf('%level') >= 0) {
         descr = descr.replace('%level', pretty_print_number(level));
     }
     while(descr.indexOf('%gamebucks') >= 0) {
         descr = descr.replace('%gamebucks', player.get_any_abtest_value('gamebucks_ui_name', gamedata['store']['gamebucks_ui_name']));
     }
-    if(descr.indexOf('%modstats') >= 0) {
+    while(descr.indexOf('%modstats') >= 0) {
         var ui_modstat_buff_list = [], ui_modstat_nerf_list = [];
         goog.array.forEach(spec['effects'], function(eff) {
             var ui_effect = ModChain.display_modstat_effect(eff, level);
