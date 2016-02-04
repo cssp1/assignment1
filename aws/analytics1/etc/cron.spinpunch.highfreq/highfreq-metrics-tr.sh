26f055d7b7761ac58e176b1cb4d713072ee54161
Dan Maas <dmaas@spinpunch.com>
2016-02-05 03:36:38 +0900

    remove experimental nosql upcache dump option from highfreq metrics

--- aws/analytics1/etc/cron.spinpunch.highfreq/highfreq-metrics-tr.sh
+++ aws/analytics1/etc/cron.spinpunch.highfreq/highfreq-metrics-tr.sh
@@ -9,6 +9,6 @@ AWSSECRET=/home/ec2-user/.ssh/${HOST}-awssecret
 (cd $GAME_DIR/gameserver && ./make-gamedata.sh -n -u > /dev/null)
 
 (cd $GAME_DIR/gameserver && nice ./dump_userdb.py --cache-only \
-    --cache-segments 128 --parallel 16 --to-mongodb tr_upcache --nosql-deltas-only --s3-userdb \
+    --cache-segments 128 --parallel 16 --s3-userdb \
     --cache-read logs/thunderrun-upcache --from-s3-bucket spinpunch-upcache --from-s3-keyfile $AWSSECRET \
     --cache-write logs/thunderrun-upcache --to-s3-bucket spinpunch-upcache --to-s3-keyfile $AWSSECRET) > /dev/null
