b77fb26f50d796014b98cb10316359e31672b205
Dan Maas <dmaas@spinpunch.com>
2016-02-06 21:08:11 -0600

    use /dev/shm for playerdb backup scratch space since it's bounded in size now

--- aws/analytics1/etc/cron.spinpunch.daily/backup-mf-player-data.sh
+++ aws/analytics1/etc/cron.spinpunch.daily/backup-mf-player-data.sh
@@ -3,7 +3,7 @@
 . /etc/spinpunch
 
 GAME_DIR=/home/ec2-user/marsfrontier
-export TMPDIR="${SPIN_TMPDIR}"
+export TMPDIR="${SPIN_SMALL_TMPDIR}"
 
 # perform S3 backup only, leave local backup for prod.spinpunch.com
 (cd $GAME_DIR/gameserver && nice ./backup-data.py --quiet --parallel 16 --local 0 --s3 1) > /dev/null
