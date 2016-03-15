34d87f6bd384c6e22b5f171e546dee4513faf4aa
Dan Maas <dmaas@spinpunch.com>
2016-03-15 12:14:39 +0900

    replays: show timer in replay overlay

--- gameclient/clientcode/BattleReplayGUI.js
+++ gameclient/clientcode/BattleReplayGUI.js
@@ -54,14 +54,6 @@ BattleReplayGUI.invoke = function(player, link_url) {
     install_child_dialog(dialog);
     dialog.modal = false;
     dialog.widgets['close_button'].onclick = close_parent_dialog;
-    dialog.ondraw = function(dialog) {
-        var player = dialog.user_data['player'];
-        dialog.xy = [Math.floor((SPUI.canvas_width - dialog.wh[0])/2),
-                     Math.floor((0.3*SPUI.canvas_height - dialog.wh[1])/2)];
-        var s = dialog.data['widgets']['description']['ui_name'];
-        dialog.widgets['description'].str = s.replace('%cur_tick', pretty_print_number(player.cur_tick())).replace('%num_ticks', pretty_print_number(player.num_ticks()));
-
-    };
     if(link_url) {
         dialog.widgets['share_button'].show = true;
         dialog.widgets['share_button'].onclick = function(w) {
@@ -78,5 +70,23 @@ BattleReplayGUI.invoke = function(player, link_url) {
     } else {
         dialog.widgets['share_button'].show = false;
     }
+    dialog.ondraw = BattleReplayGUI.update;
     return dialog;
+};
+
+/** @param {!SPUI.Dialog} dialog */
+BattleReplayGUI.update = function(dialog) {
+    var replay_player = dialog.user_data['player'];
+    dialog.xy = [Math.floor((SPUI.canvas_width - dialog.wh[0])/2),
+                 Math.floor((0.3*SPUI.canvas_height - dialog.wh[1])/2)];
+    // update description text
+    var total_seconds = replay_player.num_ticks() * TICK_INTERVAL;
+    var total_minutes = Math.floor(total_seconds/60.0);
+    var total_time = total_minutes.toFixed(0) + ':' + pad_with_zeros((total_seconds%60.0).toFixed(0), 2);
+
+    var cur_seconds = Math.min(replay_player.cur_tick() * TICK_INTERVAL, Math.floor(total_seconds)); // chop off fractional part at end
+    var cur_minutes = Math.floor(cur_seconds/60.0);
+    var cur_time = cur_minutes.toFixed(0) + ':' + pad_with_zeros(Math.floor(cur_seconds%60.0).toFixed(0), 2); // +'.'+pad_with_zeros(((cur_seconds*100)%100).toFixed(0), 2);
+    var s = dialog.data['widgets']['description']['ui_name'];
+    dialog.widgets['description'].str = s.replace('%cur_time', cur_time).replace('%total_time', total_time);
 };
