49fa6957c28bf071f2edc6433cae31680eb231aa
Dan Maas <dmaas@spinpunch.com>
2016-07-30 04:19:47 -0400

    clean_backups.py: update for battlehouse deployment

--- gameserver/clean_backups.py
+++ gameserver/clean_backups.py
@@ -11,6 +11,8 @@ import SpinParallel
 TASKS = [
     {'bucket': 'spinpunch-forums', 'pattern': 'spinpunch-forums-backup-YYYYMMDD.tar.gz'},
     {'bucket': 'battlehouse-vbulletin-backups', 'pattern': 'battlehouse-vbulletin-YYYYMMDD.tar.gz'},
+    {'bucket': 'battlehouse-wordpress-backups', 'pattern': 'battlehouse-wordpress-YYYYMMDD.tar.gz'},
+    {'bucket': 'battlehouse-mattermost-backups', 'pattern': 'battlehouse-mattermost-YYYYMMDD.tar.gz'},
     {'bucket': 'spinpunch-mattermost-backups', 'pattern': 'spinpunch-mattermost-YYYYMMDD.tar.gz'},
     {'bucket': 'spinpunch-www', 'pattern': 'about-battlehouse-backup-YYYYMMDD.tar.gz'},
     {'bucket': 'spinpunch-www', 'pattern': 'spinpunch-www-backup-YYYYMMDD.tar.gz'},
