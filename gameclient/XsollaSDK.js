a8cc7a3c6e36daae4389dbda792857db0c1ace7a
Dan Maas <dmaas@spinpunch.com>
2016-09-08 18:48:13 +0200

    Update Xsolla SDK version

--- gameclient/XsollaSDK.js
+++ gameclient/XsollaSDK.js
@@ -3,7 +3,7 @@ var spin_xsolla_sdk_loaded = false;
 (function(d){
     var js, id = "xsolla-jssdk"; if (d.getElementById(id)) {return;}
     js = d.createElement("script"); js.id = id; js.async = true;
-    js.src = "//static.xsolla.com/embed/paystation/1.0.1/widget.min.js";
+    js.src = "//static.xsolla.com/embed/paystation/1.0.6/widget.min.js";
     js.addEventListener('load', function(e) {
         spin_xsolla_sdk_loaded = true;
     }, false);
