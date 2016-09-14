238dafedf064f69508fef8b6f71cedec435518e9
Dan Maas <dmaas@spinpunch.com>
2016-09-14 11:47:29 +0200

    disregard missing hipchat.token key file

--- aws/prod/etc/cron.spinpunch.daily/01-dev-reminders.sh
+++ aws/prod/etc/cron.spinpunch.daily/01-dev-reminders.sh
@@ -2,6 +2,8 @@
 
 . /etc/spinpunch
 
-export HIPCHAT_TOKEN=`cat /home/ec2-user/.ssh/hipchat.token`
+if [ -e "/home/ec2-user/.ssh/hipchat.token" ]; then
+    export HIPCHAT_TOKEN=`cat /home/ec2-user/.ssh/hipchat.token`
+fi
 
 (cd $GAME_DIR/gameserver && ./dev_reminders.py > /dev/null)
