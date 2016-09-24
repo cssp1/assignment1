f61b551584101d666f46b73f659be875a43e5132
Dan Maas <dmaas@spinpunch.com>
2016-09-24 22:33:26 +0200

    Add GameDataVisualizer and produce fs_buildings.html

---
+++ gamedata/GameDataVisualizer.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015 Battlehouse Inc. All rights reserved.
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file.
+
+# pretty-print gamedata for easier human analysis
+
+from GameDataUtil import get_leveled_quantity, get_max_level
+from cStringIO import StringIO
+
+def pretty_print_time_brief(sec):
+    d = int(sec/86400)
+    sec -= 86400*d
+    h = int(sec/3600)
+    sec -= 3600*h
+    m = int(sec/60)
+    sec -= 60*m
+    ret = ''
+    if d > 0:
+        ret += '%dd' % d
+    if h > 0:
+        ret += '%dh' % h
+    if d <= 0 and h < 4:
+        ret += '%dm' % m
+    #ret += '%ds' % sec
+    return ret
+
+def pretty_print_qty(qty):
+    if qty >= 1000000:
+        return '%.1fM' % (qty/1000000.0)
+    elif qty >= 10000:
+        return '%dK' % (qty//1000)
+    elif qty >= 1000:
+        return '%.1fK' % (qty/1000.0)
+    return str(qty)
+
+def pretty_print_stat(key, val):
+    if key.endswith('_time'):
+        return pretty_print_time_brief(val)
+    elif key.endswith('_days'):
+        if val < 1:
+            return '%.2fd' % val
+        else:
+            return '%.1fd' % val
+    else:
+        return pretty_print_qty(val)
+    #return val_str = '{:,}'.format(val)
+
+class Table(object):
+    """ Generic table that shows key/value stats where the values are per-level arrays.
+    Also supports showing per-tier stats, optionally.
+
+    Note: data must all consist of per-level arrays. Duplicate scalars into arrays first.
+    """
+
+    def __init__(self, title, data, tiers = None, tier_data = None):
+        self.title = title
+        self.data = data # {key: [val1, val2, val3, ...], ...}
+        if data:
+            self.max_level = len(self.data[next(self.data.iterkeys())])
+        else:
+            self.max_level = len(tiers)
+        self.tiers = tiers # [1,1,2,3,4,5, ... ] corresponding to val1, val2, ...
+        self.tier_data = tier_data
+
+    def as_html(self, fd):
+        fd.write('<h2>%s</h2>' % self.title)
+        fd.write('<table>\n')
+
+        fd.write('<thead>')
+        if self.tiers is not None:
+            fd.write('<tr class="tiers">')
+            fd.write('<th class="tiers_label">Tier</th>')
+            last_tier = self.tiers[0]
+            ncols = 1
+            # loop through levels (there can be multiple levels per tier)
+            for j in range(self.max_level):
+                tier = self.tiers[j]
+                if tier != last_tier:
+                    fd.write('<th colspan="%d">%d</th>' % (ncols-1, last_tier))
+                    ncols = 1
+                if j == self.max_level-1:
+                    fd.write('<th colspan="%d">%d</th>' % (ncols, tier))
+                last_tier = tier
+                ncols += 1
+            fd.write('</tr>\n')
+
+        if self.data:
+            fd.write('<tr class="levels">')
+            fd.write('<th>Level</th>')
+            for j in range(self.max_level):
+                fd.write('<th colspan="%d">%d</th>' % (1, j+1))
+            fd.write('</tr>\n')
+
+        fd.write('</thead>\n')
+        fd.write('<tbody>\n')
+
+        if self.tier_data: # some data are per-tier, not per-level
+            assert self.tiers is not None
+            for i, key in enumerate(sorted(self.tier_data.keys())):
+                fd.write('<tr class="stat">')
+                fd.write('<td>%s</td>' % key)
+
+                last_tier = self.tiers[0]
+                ncols = 1
+                # loop through levels (there can be multiple levels per tier)
+                for j in range(self.max_level):
+                    tier = self.tiers[j]
+
+                    colspan = None
+
+                    if tier != last_tier:
+                        colspan = ncols-1
+                        val = self.tier_data[key][last_tier-1]
+                        fd.write('<td colspan="%d">%s</td>' % (colspan, pretty_print_stat(key, val)))
+                        ncols = 1
+
+                    if j == self.max_level-1:
+                        colspan = ncols
+                        val = self.tier_data[key][tier-1]
+                        fd.write('<td colspan="%d">%s</td>' % (colspan, pretty_print_stat(key, val)))
+
+                    last_tier = tier
+                    ncols += 1
+                fd.write('</tr>\n')
+
+        for i, key in enumerate(sorted(self.data.keys())):
+            fd.write('<tr class="stat">')
+            fd.write('<td>%s</td>' % key)
+            for val in self.data[key]:
+                fd.write('<td>%s</td>' % pretty_print_stat(key, val))
+            fd.write('</tr>\n')
+
+        fd.write('</tbody>\n')
+        fd.write('</table>\n')
+
+class BuildingTable(Table):
+    FIELDS = {'max_hp','build_cost_iron','build_time',
+              'consumes_power','provides_power',
+              'production_capacity'}
+
+    def __init__(self, name, spec, tier_building = None):
+        max_level = get_max_level(spec)
+        data = {}
+        for k, v in spec.iteritems():
+            if not (k in self.FIELDS or
+                    k.startswith('produces_') or
+                    k.startswith('storage_')): continue
+            if isinstance(v, list):
+                val = v
+            else:
+                val = [v,] * max_level
+            data[k] = val
+
+        tier_data = {}
+
+        if tier_building:
+            if name == tier_building:
+                tiers = list(range(1, max_level+1))
+            elif 'requires' in spec:
+                last_tier = 1
+                tiers = []
+                for pred in spec['requires']:
+                    if pred['predicate'] == 'BUILDING_LEVEL' and \
+                       pred['building_type'] == tier_building:
+                        tier = pred['trigger_level']
+                    else:
+                        tier = last_tier
+                    tiers.append(tier)
+                    last_tier = tier
+                if 'limit' in spec and isinstance(spec['limit'], list):
+                    tier_data['limit'] = spec['limit']
+        else:
+            tiers = None
+
+        Table.__init__(self, name, data, tiers = tiers, tier_data = tier_data)
+
+def get_tier_summary(buildings, building_names, tier_building, n_tiers):
+    summary = {'power_consumed':[0]*n_tiers,
+               'power_produced':[0]*n_tiers,
+               'building_upgrade_res':[0]*n_tiers,
+               'building_upgrade_days':[0]*n_tiers,
+               'harvest_rate_daily':[0]*n_tiers,
+               'storage':[0]*n_tiers,
+               'repair_time':[0]*n_tiers}
+
+    for name in building_names:
+        data = buildings[name]
+        max_level = get_max_level(data)
+
+        level = 0
+
+        # per building instance quantities
+
+        # note: these need to continue across tiers
+        power_consumed_this_tier = 0
+        power_produced_this_tier = 0
+        harvest_rate_daily_this_tier = 0
+        storage_this_tier = 0
+        repair_time_this_tier = 0
+
+        for tier in range(1, n_tiers+1):
+
+            # these get re-initialized to zero each tier
+            upgrade_res_this_tier = 0
+            upgrade_days_this_tier = 0
+
+            # upgrade until we can upgrade no more at this tier
+            if name == tier_building:
+                max_level_this_tier = tier
+
+            elif 'requires' in data:
+                max_level_this_tier = max_level
+                for i, pred in enumerate(data['requires']):
+                    if pred['predicate'] == 'BUILDING_LEVEL' and \
+                       pred['building_type'] == tier_building:
+                        if pred['trigger_level'] > tier:
+                            max_level_this_tier = (i+1) - 1
+                            break
+
+            while level < max_level_this_tier:
+                # simulate upgrade
+                level += 1
+                upgrade_res_this_tier += data['build_cost_iron'][level-1]
+                # convert seconds to days
+                upgrade_days_this_tier += data['build_time'][level-1] / 86400.0
+                if 'consumes_power' in data:
+                    power_consumed_this_tier = max(power_consumed_this_tier, data['consumes_power'][level-1])
+                if 'provides_power' in data:
+                    power_produced_this_tier = max(power_produced_this_tier, data['provides_power'][level-1])
+                if 'produces_iron' in data:
+                    # convert per-hour to per-day
+                    harvest_rate_daily_this_tier = max(harvest_rate_daily_this_tier, 24*data['produces_iron'][level-1])
+                if 'storage_iron' in data:
+                    storage_this_tier = max(storage_this_tier, data['storage_iron'][level-1])
+                if 'repair_time' in data and isinstance(data['repair_time'], list):
+                    repair_time_this_tier = max(repair_time_this_tier, data['repair_time'][level-1])
+
+            limit = data.get('limit', 1)
+            if isinstance(limit, list):
+                limit = limit[tier-1]
+
+            summary['building_upgrade_res'][tier-1] += limit * upgrade_res_this_tier
+            summary['building_upgrade_days'][tier-1] += limit * upgrade_days_this_tier
+            summary['power_consumed'][tier-1] += limit * power_consumed_this_tier
+            summary['power_produced'][tier-1] += limit * power_produced_this_tier
+            summary['harvest_rate_daily'][tier-1] += limit * harvest_rate_daily_this_tier
+            summary['storage'][tier-1] += limit * storage_this_tier
+            # note: all repair happens in parallel
+            summary['repair_time'][tier-1] = max(summary['repair_time'][tier-1], repair_time_this_tier)
+
+    return summary
+
+class PageOfTables(object):
+    def __init__(self, table_list):
+        self.table_list = table_list
+    def as_html(self, fd):
+        fd.write('''
+<html>
+<head>
+<style>
+body {
+    background: #ffffff;
+    font-family: sans-serif;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+    table-layout: fixed;
+}
+table, th, td { border: 1px solid #808080; }
+.tiers th {
+    background-color: #4CAF50;
+    color: white;
+}
+.tiers th.tiers_label {
+    width: 120px;
+}
+.levels th {
+    background-color: #AFAF50;
+    color: white;
+}
+.stat td {
+    font-family: sans-serif;
+    font-size: 75%;
+    text-align: right;
+}
+th, td {
+    padding: 3px;
+    text-align: left;
+}
+tr:nth-child(even) { background-color: #f2f2f2 }
+</style>
+</head>
+<body>
+''')
+        for table in self.table_list:
+            table.as_html(fd)
+        fd.write('''
+</body>
+</html>
+''')
+
+if __name__ == '__main__':
+    import sys, os, getopt
+    import AtomicFileWrite
+    import SpinJSON
+
+    out_filename = '-'
+    tier_building = None
+
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'o:', ['tier-building='])
+    for key, val in opts:
+        if key == '-o': out_filename = val
+        elif key == '--tier-building': tier_building = val
+
+    if len(args) < 1:
+        print 'usage: GameDataVisualizer.py -o [output] buildings.json [--tier-building=toc]'
+        sys.exit(1)
+
+    buildings = SpinJSON.load(open(args[0]))
+
+    if out_filename == '-':
+        out_fd = sys.stdout
+        out_atom = None
+    else:
+        out_atom = AtomicFileWrite.AtomicFileWrite(out_filename, 'w', ident=str(os.getpid()))
+        out_fd = out_atom.fd
+
+    # get list of buildings to process
+    building_names = sorted(buildings.keys())
+
+    # remove non-relevant buildings
+    building_names = filter(lambda name: (not buildings[name].get('developer_only') or \
+                              name in ('weapon_factory', 'weapon_lab', 'turret_emplacement')),
+                            building_names)
+
+    if tier_building:
+        # put tier building first
+        building_names.remove(tier_building)
+        building_names = [tier_building,]+building_names
+
+    table_list = [BuildingTable(name, buildings[name], tier_building) for name in building_names]
+
+    if tier_building:
+        # add summary data
+        n_tiers = len(buildings[tier_building]['build_time'])
+        summary_data = get_tier_summary(buildings, building_names, tier_building, n_tiers)
+        table_list = [Table('SUMMARY', {}, range(1, n_tiers+1), summary_data),] + table_list
+
+    PageOfTables(table_list).as_html(out_fd)
+
+    if out_atom:
+        out_atom.complete()
