83c9878125417406662e2d92e0c1255741af7508
Dan Maas <dmaas@spinpunch.com>
2016-09-29 15:43:56 +0200

    all-to-mysql: update analytics update reminder schedule

--- gameserver/all-to-mysql.sh
+++ gameserver/all-to-mysql.sh
@@ -106,11 +106,12 @@ if [[ "$FREQ" == "daily" ]]; then
   run_it ./acquisitions_to_sql.py -q # requires upcache and analytics-views
 
   # send SpinReminder notification
-  # TR: Mon/Tue/Wed/Thu/Fri, others: Mon/Thu
+  # TR: Mon/Tue/Wed/Thu/Fri, DV: Mon/Thu, others: Thu only
   # DAY_OF_WEEK is (1..7); 1 is Monday
 
   if [[ (("${GAME_ID}" == "tr") && ($RUN_START_DAY_OF_WEEK -le 5)) || \
-        ($RUN_START_DAY_OF_WEEK -eq 1) || ($RUN_START_DAY_OF_WEEK -eq 4) ]]; then
+        (("${GAME_ID}" == "dv") && ($RUN_START_DAY_OF_WEEK -eq 1)) || \
+        ($RUN_START_DAY_OF_WEEK -eq 4) ]]; then
       ./SpinReminders.py --from "all-to-mysql.sh" --subject "${GAME_ID} daily metrics" --body "daily SQL analytics update is finished. Input data was complete starting from ${RUN_START_UI_TIME}." \
                          --recipients "`./SpinConfig.py --getvar analytics_recipients`"
   fi
