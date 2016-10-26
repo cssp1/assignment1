02aa14e59391a8de305e27cec3ea1e8db40c8b1f
Dan Maas <dmaas@spinpunch.com>
2016-10-26 06:09:05 +0200

    load Battlehouse CSS stylesheet

--- gameclient/BattlehouseSDK.js
+++ gameclient/BattlehouseSDK.js
@@ -1,3 +1,5 @@
+<link rel="stylesheet" id="google_fonts-css" href="https://fonts.googleapis.com/css?family=Lato&amp;ver=4.5.3" type="text/css" media="all">
+<link rel="stylesheet" href="$BH_LOGIN_PATH$/client.css" type="text/css" media="all">
 <script type="text/javascript">(function() {
     var s = document.createElement('script');
     s.type = 'text/javascript'; s.async = true;
