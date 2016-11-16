fc49e9ed7da19a1395fdeb8a627aca99bbf16bea
Dan Maas <dmaas@spinpunch.com>
2016-11-16 02:38:07 +0100

    comment player alias disallowed_chars to keep all duplicates in sync

--- gameserver/server.py
+++ gameserver/server.py
@@ -192,7 +192,7 @@ tag_chars_ext = name_chars_simple + \
 name_chars_ext = tag_chars_ext + \
                  [' ', "'"]
 descr_disallowed_chars = ['\n', '\t', '\r']
-alias_disallowed_chars = ['\n', '\t', '\r', '\\', '/', ' ', '.', ':', ';', '+', '*', '(', ')', '<', '>', '[', ']', '{', '}', ',', '|', '"', "'", '_', '&', '^', '%', '$', '#', '@', '!', '~', '?', '`', '\0'] # keep in sync with main.js and errors.json ALIAS_BAD
+alias_disallowed_chars = ['\n', '\t', '\r', '\\', '/', ' ', '.', ':', ';', '+', '*', '(', ')', '<', '>', '[', ']', '{', '}', ',', '|', '"', "'", '_', '&', '^', '%', '$', '#', '@', '!', '~', '?', '`', '\0'] # keep in sync with loginserver.py, server.py, main.js, and errors.json ALIAS_BAD
 
 def is_valid_alliance_descr(name):
     if len(name) > 256: return False
