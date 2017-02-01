02e7c561578bdbb6f9911b008411fadb0e74a5ff
Dan Maas <dmaas@spinpunch.com>
2017-02-01 10:43:13 +0100

    FBShare: remove legacy "feed" API usage

--- gameclient/clientcode/FBShare.js
+++ gameclient/clientcode/FBShare.js
@@ -11,6 +11,13 @@ goog.provide('FBShare');
 
 goog.require('SPFB');
 
+/** @return {boolean}
+    Check if FB Sharing is available. Works when the game is hosted on FB Canvas,
+    AND when on Battlehouse. */
+FBShare.supported = function() {
+    return (spin_frame_platform == 'fb' || (spin_frame_platform == 'bh' && spin_battlehouse_fb_app_id));
+};
+
 /** @typedef {{ref: string,
                name: string,
                link_qs: (Object.<string,string>|undefined),
@@ -24,49 +31,6 @@ FBShare.default_picture = function() {
     var common = /** @type {string} */ (gamedata['virals']['common_image_path']);
     common += /** @type {string} */ (gamedata['virals']['default_image']);
     return common;
-};
-
-/** Facebook's old legacy "feed" API
-    @suppress {reportUnknownTypes} - Closure doesn't deal with the nested callbacks well
-    @param {!FBShare.Options} p_options */
-FBShare.invoke_feed = function(p_options) {
-    if(!spin_facebook_enabled) { console.log('FBShare.invoke_feed: '+p_options.ref); return; }
-
-    call_with_facebook_permissions('publish_actions',
-        (function(options) { return function() {
-            metric_event('7270_feed_post_attempted', {'method':options.ref, 'api':'feed', 'sum': player.get_denormalized_summary_props('brief')});
-            var url = 'https://apps.facebook.com/'+spin_app_namespace+'/?spin_ref='+options.ref+'&spin_ref_user_id='+spin_user_id.toString();
-            if(options.link_qs) {
-                for(var k in options.link_qs) {
-                    url += '&'+k+'='+encodeURIComponent(options.link_qs[k]);
-                }
-            }
-            if(player.is_developer()) {
-                console.log(url);
-            }
-
-            SPFB.ui({'method': 'feed',
-                     'name': options.name || null,
-                     'message': options.message || null,
-                     'description': options.description || null,
-                     'link': url,
-                     'picture': options.picture || FBShare.default_picture(),
-                     'ref': options.ref,
-                     'show_error': !spin_secure_mode || player.is_developer()},
-
-                    /** @param {!FBShare.Options} _options */
-                    (function (_options) {
-                        return /** @type {function(?Object.<string,string>)} */ (function(result) {
-                            if(result && ('post_id' in result)) {
-                                metric_event('7271_feed_post_completed',
-                                             {'method':_options.ref, 'api':'feed',
-                                              'facebook_post_id':result['post_id'],
-                                              'sum': player.get_denormalized_summary_props('brief')
-                                             });
-                            }
-                        }); })(options)
-                   );
-        }; })(p_options));
 };
 
 /** Facebook's new "Share" dialog
@@ -113,17 +77,10 @@ FBShare.invoke_share = function(options) {
            );
 };
 
-/** Old legacy method
+/** Main entry point
     @param {!FBShare.Options} options */
 FBShare.invoke = function(options) {
+    if(!FBShare.supported()) { throw Error('FBShare not supported on this platform'); }
     if(options.ref.length > 15) { throw Error('ref too long: "'+options.ref+'"'); }
-
-    var api = /** @type {?string} */ (gamedata['virals']['facebook_api']) || 'feed';
-    if(api == 'feed') {
-        FBShare.invoke_feed(options);
-    } else if(api == 'share') {
-        FBShare.invoke_share(options);
-    } else {
-        throw Error('unknown api '+api);
-    }
+    return FBShare.invoke_share(options);
 };
