9596a161911ddbf4fc6f612ebc73c85388bb3e3e
Dan Maas <dmaas@spinpunch.com>
2017-04-17 20:31:06 +0200

    Notification2: separate STREAM_ALWAYS from STREAM_PRIORITY

--- gameserver/Notification2.py
+++ gameserver/Notification2.py
@@ -24,7 +24,8 @@ max_per_day = { USER_NEW: 1,
 
 STREAM_RETENTION = 'RETENTION'
 STREAM_PROGRESSION = 'PROGRESSION'
-STREAM_URGENT = 'URGENT' # got attacked etc
+STREAM_URGENT = 'URGENT' # got attacked etc - send once per logout
+STREAM_ALWAYS = 'ALWAYS' # always send, even repeatedly per logout
 
 # when no timezone is known, assume center-of-gravity in mid US
 DEFAULT_TIMEZONE = -5
@@ -35,16 +36,18 @@ def ref_to_stream(ref):
             'you_got_attacked': STREAM_URGENT,
             'incoming_raid': STREAM_URGENT,
             'raid_complete': STREAM_URGENT,
-            'you_sent_gift_order': STREAM_URGENT,
-            'you_got_gift_order': STREAM_URGENT,
-            'your_gift_order_was_received': STREAM_URGENT,
-            'alliance_promoted': STREAM_URGENT,
-            'alliance_demoted': STREAM_URGENT,
-            'bh_invite_accepted_sender': STREAM_URGENT,
-            'bh_invite_accepted_target': STREAM_URGENT,
-            'bh_invite_completed_sender': STREAM_URGENT,
-            'bh_invite_completed_target': STREAM_URGENT,
+
+            'you_sent_gift_order': STREAM_ALWAYS,
+            'you_got_gift_order': STREAM_ALWAYS,
+            'your_gift_order_was_received': STREAM_ALWAYS,
+            'alliance_promoted': STREAM_ALWAYS,
+            'alliance_demoted': STREAM_ALWAYS,
+            'bh_invite_accepted_sender': STREAM_ALWAYS,
+            'bh_invite_accepted_target': STREAM_ALWAYS,
+            'bh_invite_completed_sender': STREAM_ALWAYS,
+            'bh_invite_completed_target': STREAM_ALWAYS,
             'bh_invite_daily_gift': STREAM_URGENT,
+
             '168h': STREAM_RETENTION,
             'retain_168h': STREAM_RETENTION,
             'retain_167h': STREAM_RETENTION,
@@ -75,15 +78,15 @@ def can_send(time_now, timezone, stream, ref, history, cooldowns, user_class):
     last_logout_time = history['sessions'][-1][1]
     local_time_now = time_now + timezone * 3600
 
-    if user_class is USER_NEW and stream is not STREAM_RETENTION:
-        return False, 'cannot send notification streams other than retention to TH<2 users'
+    if user_class is USER_NEW and stream not in (STREAM_RETENTION, STREAM_ALWAYS):
+        return False, 'cannot send notification streams other than RETENTION and ALWAYS to TH<2 users'
 
     # not sure about this restriction
 #    if user_class is USER_ENGAGED and stream is STREAM_PROGRESSION:
 #        return False, 'cannot send progression stream until TH 3+'
 
     # don't send notifications within 1 hour of playing, except urgent ones
-    if stream is not STREAM_URGENT:
+    if stream not in (STREAM_URGENT, STREAM_ALWAYS):
         if last_logout_time <= 0:
             return False, 'player is logged in'
         if last_logout_time >= time_now - 3600:
@@ -104,23 +107,26 @@ def can_send(time_now, timezone, stream, ref, history, cooldowns, user_class):
 
     # non-retention messages
     else:
-        # don't send more than once per stream since logout
-        if history.get('notification2:%s:last_time' % stream,-1) >= last_logout_time:
-            return False, 'a notification on this stream was already sent since last logout'
 
-        # check against daily limit
-        cdname = 'notification2_%s' % local_date(local_time_now)
-        if cdname in cooldowns:
-            stack = cooldowns[cdname].get('stack', 1)
-            if stack >= max_per_day[user_class]:
-                return False, 'max %d notification(s) per day for user class %s' % \
-                       (max_per_day[user_class], user_class)
+        if stream is not STREAM_ALWAYS:
+            # don't send more than once per stream since logout (except ALWAYS)
+            if history.get('notification2:%s:last_time' % stream,-1) >= last_logout_time:
+                return False, 'a notification on this stream was already sent since last logout'
+
+            # check against daily limit
+            cdname = 'notification2_%s' % local_date(local_time_now)
+            if cdname in cooldowns:
+                stack = cooldowns[cdname].get('stack', 1)
+                if stack >= max_per_day[user_class]:
+                    return False, 'max %d notification(s) per day for user class %s' % \
+                           (max_per_day[user_class], user_class)
 
         # auto-mute - check for too many unacked notifications with this ref
         if ref:
-            max_unacked = {STREAM_URGENT: 5}.get(stream, 3)
+            max_unacked = {STREAM_URGENT: 5, STREAM_ALWAYS: 5}.get(stream, 3)
             if history.get('notification2:%s:unacked' % ref, 0) >= max_unacked:
                 return False, 'player has ignored too many notifications with ref "%s" (auto-muted)' % ref
+
     return True, None
 
 def _record_send_history(time_now, param, history):
