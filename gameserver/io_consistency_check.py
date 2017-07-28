53c259caab31c2a2e27a70453487dfd2d95a1466
Dan Maas <dmaas@spinpunch.com>
2017-07-28 04:52:19 +0200

    io_consistency_check: handle AI resets across week boundaries

--- gameserver/io_consistency_check.py
+++ gameserver/io_consistency_check.py
@@ -7,6 +7,9 @@
 import sys, getopt
 import ANSIColor
 
+def week_of(ts):
+    return (ts - 1337274000) // (7*86400)
+
 player_id_to_gen = {}
 player_id_to_time = {}
 
@@ -22,11 +25,18 @@ opts, args = getopt.gnu_getopt(sys.argv[1:], 'v', ['verbose'])
 for key, val in opts:
     if key == '-v' or key == '--verbose': verbose += 1
 
+last_event_time = -1
+
 for line in sys.stdin.xreadlines():
     event_time_rounded, event_date, event_daytime, event_time, \
                         server_name, category, action, id, gen = line.strip().split(' ')
     event_time = float(event_time)
     gen = int(gen)
+
+    # re-initialize AIs on week boundaries
+    if last_event_time > 0 and week_of(event_time) != week_of(last_event_time):
+        ai_id_to_gen.clear()
+        ai_id_to_time.clear()
 
     count += 1
 
@@ -64,5 +74,7 @@ for line in sys.stdin.xreadlines():
         print line.strip()
         print ', '.join(map(ANSIColor.red, err_list) + map(ANSIColor.yellow, warn_list))
         print
+
+    last_event_time = event_time
 
 print count, 'actions', errors, 'errors', warnings, 'warnings'
