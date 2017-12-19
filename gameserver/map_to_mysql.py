d815728a51f517babeeec8ae36222a6e81cdc858
Dan Maas <dmaas@spinpunch.com>
2017-12-19 13:48:52 -0800

    map_to_mysql: add is_active flag to player bases to track active population separately from churned bases

--- gameserver/map_to_mysql.py
+++ gameserver/map_to_mysql.py
@@ -23,6 +23,7 @@ def map_summary_schema(sql_util): return {
                ('base_type', 'VARCHAR(8) NOT NULL'),
                ('base_template', 'VARCHAR(32)'),
                ('townhall_level', 'INT'),
+               ('is_active', sql_util.bit_type()),
                ('count', 'INT')],
     'indices': {'by_time': {'keys': [('time','ASC')]}}
     }
@@ -42,6 +43,7 @@ def map_features_schema(sql_util): return {
     'fields': [('time', 'INT8 NOT NULL'),
                ('region_id', 'VARCHAR(16) NOT NULL'),
                ('base_id', 'VARCHAR(32) NOT NULL'),
+               ('last_mtime', 'INT8'),
                ('base_map_loc_x', 'SMALLINT NOT NULL'),
                ('base_map_loc_y', 'SMALLINT NOT NULL'),
                ('base_landlord_id', 'INT NOT NULL'),
@@ -140,6 +142,7 @@ if __name__ == '__main__':
                     features_sql.append([('time', time_now),
                                          ('region_id',name),
                                          ('base_id',feature['base_id']),
+                                         ('last_mtime',feature.get('last_mtime',None)),
                                          ('base_map_loc_x',feature['base_map_loc'][0]),
                                          ('base_map_loc_y',feature['base_map_loc'][1]),
                                          ('base_landlord_id',feature['base_landlord_id']),
@@ -168,17 +171,22 @@ if __name__ == '__main__':
                                                ('base_template',row['_id']),
                                                ('count',row['count'])] for row in count_by_template])
 
-            # count player bases by townhall level
+            # count player bases by townhall level, separating active/non-active status
+            active_time = time_now - 3 * 86400 # arbitrarily say last_mtime within 3 days counts as "active"
             count_by_townhall = nosql_client.region_table(name, 'map').aggregate([{'$match':{'base_type':'home'}},
-                                                                                  {'$project':{gamedata['townhall']+'_level':1}},
-                                                                                  {'$group':{'_id': '$'+gamedata['townhall']+'_level',
+                                                                                  {'$project':{'townhall_level': '$'+gamedata['townhall']+'_level',
+                                                                                               'is_active': {'$gte': ['$last_mtime', active_time]}
+                                                                                               }},
+                                                                                  {'$group':{'_id': {'townhall_level': '$townhall_level',
+                                                                                                     'is_active': '$is_active'},
                                                                                              'count': {'$sum':1}}}])
             if not dry_run:
                 sql_util.do_insert_batch(cur, map_summary_table,
                                          [[('time',time_now),
                                            ('region_id',name),
                                            ('base_type','home'),
-                                           ('townhall_level',row['_id']),
+                                           ('townhall_level',row['_id']['townhall_level']),
+                                           ('is_active',row['_id']['is_active']),
                                            ('count',row['count'])] for row in count_by_townhall])
 
 
