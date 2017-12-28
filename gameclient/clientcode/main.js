6ea510a2ccd2787b56588274a5a03ebd8f04c22c
Dan Maas <dmaas@spinpunch.com>
2017-12-27 20:34:05 -0800

    add estimated speedup time to "Ask Clan for Help" tooltip

--- gameclient/clientcode/main.js
+++ gameclient/clientcode/main.js
@@ -20389,7 +20389,10 @@ function do_invoke_speedup_dialog(kind) {
             dialog.widgets['help_request_button'].tooltip.str = pred.ui_describe(player);
             dialog.widgets['help_request_button'].tooltip.text_color = SPUI.error_text_color;
         } else {
-            dialog.widgets['help_request_button'].tooltip.str = spell['ui_tooltip'];
+            var estimated_speedup = Math.min(selection.unit.upgrade_time_left(),
+                                             Math.max(gamedata['alliance_help_speedup_min_time'],
+                                                      Math.floor(gamedata['alliance_help_speedup_fraction'] * selection.unit.upgrade_total_time)));
+            dialog.widgets['help_request_button'].tooltip.str = spell['ui_tooltip'].replace('%est', pretty_print_time(estimated_speedup));
             dialog.widgets['help_request_button'].onclick = function(w) {
                 var s = gamedata['strings']['help_request_confirm'];
                 invoke_child_message_dialog(s['ui_title'], s['ui_description']
