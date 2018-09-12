6285cda7895689dffa92c2d664f29271b65a87cc
Dan Maas <dmaas@spinpunch.com>
2018-09-11 21:25:34 -0700

    update gamedata diffing tools to handle store.json

--- gamedata/diff.py
+++ gamedata/diff.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 
+# this tool compares two gamedata.json files and reports any differences found.
+
 import GameDataUtil
 
+def diff_recursive(a, b):
+    ret = None
+    if isinstance(a, dict):
+        if not isinstance(b, dict):
+            return 'type mismatch, type(a)=%s, type(b)=%s' % (type(a), type(b))
+        ret = {}
+        for k, v in a.iteritems():
+            x = diff_recursive(a[k], b.get(k, None))
+            if x:
+                ret[k] = x
+        return ret
+    elif isinstance(a, list):
+        if not isinstance(b, list):
+            return 'type mismatch, type(a)=%s, type(b)=%s' % (type(a), type(b))
+        if len(b) != len(a):
+            return 'list length mismatch, len(a)=%d len(b)=%d' % (len(a), len(b))
+        ret = {}
+        for i, elem in enumerate(a):
+            x = diff_recursive(a[i], b[i])
+            if x:
+                ret['[%d]' % i] = x
+        return ret
+    else:
+        if a != b:
+            return '%s != %s' % (a, b)
+
 if __name__ == '__main__':
-    import sys, os, getopt
+    import sys, getopt
     import SpinJSON
 
     opts, args = getopt.gnu_getopt(sys.argv[1:], '', [])
@@ -25,8 +53,18 @@ if __name__ == '__main__':
         if diff:
             ret[top] = diff
 
+    diff = diff_recursive(gamedata_a['store'], gamedata_b['store'])
+    if diff:
+        ret['store'] = diff
+
     crafting_recipes_diff = GameDataUtil.diff_game_objects(gamedata_a['crafting']['recipes'], gamedata_b['crafting']['recipes'])
     if crafting_recipes_diff:
         ret['crafting'] = {'recipes': crafting_recipes_diff}
 
-    SpinJSON.dump(ret, sys.stdout, pretty = True, newline = True)
+    if ret:
+        print 'Differences found:'
+        SpinJSON.dump(ret, sys.stdout, pretty = True, newline = True)
+        sys.exit(1)
+    else:
+        print 'Files are identical!'
+        sys.exit(0)
