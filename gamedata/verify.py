512c18df84162a9a618adc86d9a8052dfd208f78
James Davis <james.davis@dfs.virginia.gov>
2019-09-06 15:35:13 -0400

    [REVIEW] - Verify - Check for splash_damage and blast in TR/DV

--- gamedata/verify.py
+++ gamedata/verify.py
@@ -958,6 +958,10 @@ def check_spell(spellname, spec):
         if spec.get('name',None) != spellname.split(':')[1]:
             error |= 1; print '%s: "name" needs to be set to "%s"' % (spellname, spellname.split(':')[1])
 
+    if if gamedata['id'] in ('tr','dv') and 'splash_range' in spec and 'damage' in spec and spec['damage'] != 0 and 'blast' not in spec['damage_vs']:
+        error |= 1; print '%s: weapons with "splash_range" that have "damage" > 0 need a "blast":1 entry in "damage_vs".' % (spellname, spellname.split(':')[1])
+
+
     for EFFECT in ('muzzle_flash_effect', 'impact_visual_effect'):
         if EFFECT in spec:
             error |= check_visual_effect(spellname+':'+EFFECT, spec[EFFECT])
