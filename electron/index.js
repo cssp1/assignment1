3baccc0a33c2ae66c9d3b45573b355ca44b0793d
James Davis <6515965+The-Davis@users.noreply.github.com>
2020-02-22 22:17:27 -0500

    Updates with keyboard shortcuts

--- electron/index.js
+++ electron/index.js
@@ -1,24 +1,40 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog } = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
 
 function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 1600,
-    height: 1000,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  win.on('closed', () => {
-    win = null
-  })
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 1600,
+        height: 1000,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  // and load the index.html of the app.
-  win.loadURL('https://www.battlehouse.com/play/firestrike/', {userAgent: 'Chrome'})
-  //win.loadURL('http://localhost:9091/')
-  win.once('ready-to-show', () => {
-    win.show()
-  })
+    win.setFullScreen(true);
+    win.setMenu(null);
+    win.on('closed', () => {
+        win = null;
+    })
+    electronLocalshortcut.register(win, 'Esc', () => {
+        app.quit();
+        displayKeyPress();
+    });
+
+    // and load the index.html of the app.
+    win.loadURL('https://www.battlehouse.com/play/firestrike/', {userAgent: 'bh_electron_windows'})
+    //win.loadURL('http://localhost:9091', {userAgent: 'bh_electron_windows'})
+    win.once('ready-to-show', () => {
+        win.show();
+    })
+}
+
+function displayKeyPress () {
+    dialog.showMessageBox('Keypress detected');
 }
 
 app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit();
+})
