ca8ced066a859fde8ae18a30d011d104237f7468
James Davis <6515965+The-Davis@users.noreply.github.com>
2021-02-07 12:28:58 -0500

    Quick debug for output on live

--- gameserver/check_player.py
+++ gameserver/check_player.py
@@ -679,6 +679,10 @@ if __name__ == '__main__':
             prior_alt_violations = 0
             prior_vpn_violations = 0
             for entry in player['history']['customer_support']:
+                if entry['method'] == 'TRIGGER_COOLDOWN':
+                    print '---DEBUG HISTORY OUTPUT---'
+                    print entry
+                    break
                 if entry['method'] == 'TRIGGER_COOLDOWN' and entry.get('name') == "chat_abuse_violation": prior_chat_violations += 1
                 if entry['method'] == 'TRIGGER_COOLDOWN' and entry.get('name') == "alt_account_violation": prior_alt_violations += 1
                 if entry['method'] == 'TRIGGER_COOLDOWN' and entry.get('name') == "vpn_login_violation": prior_vpn_violations += 1
