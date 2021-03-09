36fb011e3927a992a601d8aaa7c158cb1f8607c1
James Davis <6515965+The-Davis@users.noreply.github.com>
2021-03-09 14:06:33 -0500

    Add alias tracking metrics

--- gameserver/server.py
+++ gameserver/server.py
@@ -28814,14 +28814,23 @@ class GAMEAPI(resource.Resource):
         if show_battle_history:
             retmsg.append(["SHOW_BATTLE_HISTORY"])
 
-        if session.player.alias and Predicates.read_predicate(gamedata['server'].get('reset_invalid_alias_on_login',{'predicate':'ALWAYS_FALSE'})).is_satisfied2(session, session.player, None, override_time = None) and not is_valid_alias(session.player.alias):
-            session.player.alias = None
-            new_ui_name = session.user.get_real_name()
-            self.update_player_cache_ui_name(new_ui_name)
+        if session.player.alias and not is_valid_alias(session.player.alias):
+            metric_event_coded(session.player.user_id, '0990_invalid_alias_detected', {'alias': session.player.alias,
+                                                                                       'receipts': session.player.history.get('money_spent', 0.00)})
 
         if session.player.alias and Predicates.read_predicate(gamedata['server'].get('warn_invalid_alias_on_login',{'predicate':'ALWAYS_FALSE'})).is_satisfied2(session, session.player, None, override_time = None) and gamedata['strings'].get('warn_invalid_alias_mail', False) and not is_valid_alias(session.player.alias):
             session.player.mailbox_append(session.player.make_system_mail(gamedata['strings']['warn_invalid_alias_mail'], replace_s = '%s' % session.player.alias))
             gamesite.exception_log.event(server_time, 'Invalid alias, %s, detected for player ID %d. Sending warning email.' % (session.player.alias, session.player.user_id ))
+            session.player.history['invalid_alias_warned'] = 1
+            metric_event_coded(session.player.user_id, '0991_invalid_alias_warned', {'alias': session.player.alias,
+                                                                                       'receipts': session.player.history.get('money_spent', 0.00)})
+
+        if session.player.alias and Predicates.read_predicate(gamedata['server'].get('reset_invalid_alias_on_login',{'predicate':'ALWAYS_FALSE'})).is_satisfied2(session, session.player, None, override_time = None) and not is_valid_alias(session.player.alias):
+            session.player.alias = None
+            new_ui_name = session.user.get_real_name()
+            self.update_player_cache_ui_name(new_ui_name)
+            metric_event_coded(session.player.user_id, '0992_invalid_alias_reset', {'alias': session.player.alias,
+                                                                                    'receipts': session.player.history.get('money_spent', 0.00)})
 
         if session.player.is_on_map():
             assert session.player.home_region and (session.player.my_home.base_region == session.player.home_region)
