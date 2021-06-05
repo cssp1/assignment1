009539335d01c5535c975b2acbae27a96e388850
James Davis <6515965+The-Davis@users.noreply.github.com>
2021-06-05 11:14:28 -0400

    Allow new auras

--- gamedata/verify.py
+++ gamedata/verify.py
@@ -866,7 +866,7 @@ def check_aura(auraname, spec, maxlevel):
                     error |= 1; print '%s: uses a server-side effect code but client != 0' % auraname
 
             # client-side codes (implemented in main.js)
-            elif effect['code'] in ('speed_boosted', 'speed_weakened', 'defense_boosted', 'defense_weakened', 'radiation_hardened', 'grounded',
+            elif effect['code'] in ('speed_boosted', 'speed_weakened', 'defense_boosted', 'defense_weakened', 'radiation_hardened', 'grounded', 'make_invuln', 'make_invuln_invisible', 'is_invuln',
                                     'frozen', 'ice_shielded', 'ice_encrusted', 'moving_in_swamp', 'swamp_shielded', 'range_booster_invisible', 'damage_weakened',
                                     'rate_of_fire_boosted', 'damage_boosted', 'range_boosted', 'on_island', 'damage_booster_invisible', 'defense_booster_invisible',
                                     'armor_boosted', 'damage_booster', 'defense_booster', 'stunned', 'disarmed', 'hacked', 'range_reduction', 'weak_zombie',
