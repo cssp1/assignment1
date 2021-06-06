f803f977ecd0b774bcefd53bb9111646bd6dc606
Dan Maas <dmaas@spinpunch.com>
2021-06-05 17:46:46 -0700

    aws/terraform/mongodb: Modernize Terraform files to v0.12+ syntax

--- aws/terraform/modules/mongodb/mongodb.tf
+++ aws/terraform/modules/mongodb/mongodb.tf
@@ -1,100 +1,113 @@
 resource "aws_iam_role" "mongodb" {
-  name = "${var.sitename}-mongodb"
+  name               = "${var.sitename}-mongodb"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [{"Action": "sts:AssumeRole", "Principal": {"Service": "ec2.amazonaws.com"}, "Effect": "Allow", "Sid": "" }]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy" "mongodb" {
-    name = "${var.sitename}-mongodb"
-    role = "${aws_iam_role.mongodb.id}"
-    policy = <<EOF
+  name   = "${var.sitename}-mongodb"
+  role   = aws_iam_role.mongodb.id
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     ${var.aws_ec2_iam_role_fragment},
     { "Effect": "Allow",
-      "Action": ["s3:ListBucket","s3:ListObjects","s3:GetObject","s3:PutObject"],
+      "Action": ["s3:ListBucket","s3:GetObject","s3:PutObject"],
       "Resource": ["arn:aws:s3:::${var.backups_bucket}*"]
     },
     { "Effect": "Allow",
-      "Action": ["s3:ListBucket","s3:ListObjects","s3:GetObject"],
-      "Resource": ["arn:aws:s3:::${var.extra_backups_bucket}*"]
+      "Action": ["s3:ListBucket","s3:GetObject"],
+      "Resource": [
+          "arn:aws:s3:::spinpunch-puppet*",
+          "arn:aws:s3:::${var.extra_backups_bucket}*"
+      ]
     }
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_instance_profile" "mongodb" {
-    name = "${var.sitename}-mongodb"
-    role = "${aws_iam_role.mongodb.name}"
+  name = "${var.sitename}-mongodb"
+  role = aws_iam_role.mongodb.name
 }
 
 # EC2 instance
 
 data "template_file" "my_cloud_init" {
-  count = "${var.n_instances}"
-  template = "${file("${path.module}/cloud-config.yaml")}"
+  count    = var.n_instances
+  template = file("${path.module}/cloud-config.yaml")
   vars = {
-    spin_maint_hour = "${count.index}"
-    spin_maint_weekday= "7"
+    spin_maint_hour    = count.index
+    spin_maint_weekday = "7"
     # storage mountpoints vary by instance type. i3 instances need /dev/nvme0n1...
-    mongodb_device = "${substr(var.mongodb_instance_type,0,1) == "i" ? "/dev/nvme0n1" : "/dev/sdx"}"
-    mongodb_backups_bucket = "${var.backups_bucket}"
-    mongodb_replica_set_name = "${var.sitename}"
-    mongodb_replica_set_serial = "${count.index}"
+    mongodb_device             = substr(var.mongodb_instance_type, 0, 1) == "i" ? "/dev/nvme0n1" : "/dev/sdx"
+    mongodb_backups_bucket     = var.backups_bucket
+    mongodb_replica_set_name   = var.sitename
+    mongodb_replica_set_serial = count.index
   }
 }
 
 data "template_cloudinit_config" "conf" {
-  count = "${var.n_instances}"
-  gzip = false
+  count         = var.n_instances
+  gzip          = false
   base64_encode = false
   part {
-    content = "${var.aws_cloud_config_head}"
+    content      = var.aws_cloud_config_head
     content_type = "text/cloud-config"
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
   part {
-    content = "${data.template_file.my_cloud_init.*.rendered[count.index]}"
+    content      = data.template_file.my_cloud_init[count.index].rendered
     content_type = "text/cloud-config"
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
   part {
-    content = "${var.aws_cloud_config_tail}"
+    content      = var.aws_cloud_config_tail
     content_type = "text/cloud-config"
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
 }
 
 resource "aws_instance" "mongodb" {
-  count = "${var.n_instances}"
+  count = var.n_instances
 
   # Amazon Linux AMI 2017.03
   # us-east-1 HVM (SSD) EBS-Backed 64-bit
-  ami = "${var.ami}"
+  ami = var.ami
+
   # note: HVM Instance Store needs different AMI
 
-  instance_type = "${var.mongodb_instance_type}"
+  instance_type               = var.mongodb_instance_type
   associate_public_ip_address = true
-  iam_instance_profile = "${aws_iam_instance_profile.mongodb.name}"
-  subnet_id = "${element(split(",", var.subnet_ids), count.index)}"
-  vpc_security_group_ids = ["${var.mongodb_security_group_id}",
-                            "${var.ssh_access_security_group_id}"]
-  key_name = "${var.key_pair_name}"
-  depends_on = ["aws_iam_role_policy.mongodb"]
-  tags = { 
-    Name = "${var.sitename}-mongodb-${count.index}"
+  iam_instance_profile        = aws_iam_instance_profile.mongodb.name
+  subnet_id                   = element(split(",", var.subnet_ids), count.index)
+  vpc_security_group_ids = [
+    var.mongodb_security_group_id,
+    var.ssh_access_security_group_id,
+  ]
+  key_name   = var.key_pair_name
+  depends_on = [aws_iam_role_policy.mongodb]
+  tags = {
+    Name      = "${var.sitename}-mongodb-${count.index}"
     spin_role = "prod-mongo"
     Terraform = "true"
   }
 
-  lifecycle = {
-    ignore_changes = ["ami", "user_data", "tags"] # must manually taint for these changes
+  lifecycle {
+    ignore_changes = [
+      ami,
+      user_data,
+      tags,
+      monitoring,
+    ] # must manually taint for these changes
   }
 
   # note: data size BFM 4.4G, TR 66G
@@ -102,29 +115,30 @@ resource "aws_instance" "mongodb" {
   # update mongodb_device below with device name!
 
   # for EBS (test) server
-#  ebs_block_device = {
-#    device_name = "/dev/sdx"
-#    volume_type = "io1"
-#    volume_size = 10
-#    iops = 100
-#  }
+  #  ebs_block_device = {
+  #    device_name = "/dev/sdx"
+  #    volume_type = "io1"
+  #    volume_size = 10
+  #    iops = 100
+  #  }
 
   # for i3 servers with NVMe ephemeral device
   # (not necessary - leave disabled?)
-#  ephemeral_block_device {
-#    device_name = "/dev/nvme0n1"
-#    no_device = "true"
-#    virtual_name = "ephemeral0"
-#  }
+  #  ephemeral_block_device {
+  #    device_name = "/dev/nvme0n1"
+  #    no_device = "true"
+  #    virtual_name = "ephemeral0"
+  #  }
 
-  user_data = "${data.template_cloudinit_config.conf.*.rendered[count.index]}"
+  user_data = data.template_cloudinit_config.conf[count.index].rendered
 }
 
 resource "cloudflare_record" "cf_mongodb" {
-  count = "${var.n_instances}"
-  domain = "${var.sitedomain}"
-  name = "${var.sitename}-${count.index}"
-  value = "${element(aws_instance.mongodb.*.public_dns, count.index)}"
-  type = "CNAME"
+  count   = var.n_instances
+  domain  = var.sitedomain
+  name    = "${var.sitename}-${count.index}"
+  value   = element(aws_instance.mongodb.*.public_dns, count.index)
+  type    = "CNAME"
   proxied = false
 }
+
--- aws/terraform/modules/mongodb/variables.tf
+++ aws/terraform/modules/mongodb/variables.tf
@@ -1,29 +1,62 @@
-variable "sitename" {}
-variable "extra_backups_bucket" { default = "example" } # gives read access to the backups bucket for this site
-variable "backups_bucket" {}
-variable "sitedomain" {}
-variable "region" {}
-variable "key_pair_name" {}
-variable "aws_cloud_config_head" {}
-variable "aws_cloud_config_tail" {}
-variable "aws_ec2_iam_role_fragment" {}
-variable "cron_mail_sns_topic" {}
-variable "vpc_id" {}
+variable "sitename" {
+}
+
+variable "extra_backups_bucket" { # gives read access to the backups bucket for this site
+  default = "example"
+}
+
+variable "backups_bucket" {
+}
+
+variable "sitedomain" {
+}
+
+variable "region" {
+}
+
+variable "key_pair_name" {
+}
+
+variable "aws_cloud_config_head" {
+}
+
+variable "aws_cloud_config_tail" {
+}
+
+variable "aws_ec2_iam_role_fragment" {
+}
+
+variable "cron_mail_sns_topic" {
+}
+
+variable "vpc_id" {
+}
+
 variable "subnet_ids" {
   description = "Comma-separated list of VPC subnets, corresponding to the availability_zones array"
 }
+
 variable "availability_zones" {
   description = "Comma-separated list of AWS availability zones to use, including region prefix"
   # e.g. "us-east-1a,us-east-1c"
 }
+
 variable "mongodb_instance_type" {
   description = "AWS EC2 instance type for MongoDB"
   # use t2.medium for testing, i3.large for production
 }
+
 variable "n_instances" {
   description = "Number of MongoDB server instances"
-  default = 1
+  default     = 1
 }
-variable "ami" {}
-variable "ssh_access_security_group_id" {}
-variable "mongodb_security_group_id" {}
+
+variable "ami" {
+}
+
+variable "ssh_access_security_group_id" {
+}
+
+variable "mongodb_security_group_id" {
+}
+
