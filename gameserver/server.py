3386aa479b7fe5b5108d218aab42255fe10d4e91
James Davis <6515965+The-Davis@users.noreply.github.com>
2021-12-24 21:48:42 -0500

    Improve debug message

--- gameserver/server.py
+++ gameserver/server.py
@@ -27305,7 +27305,7 @@ class GAMEAPI(resource.Resource):
 
         killer_obj = session.get_object(killer_info['id'])
         if obj.owner == killer_obj.owner:
-            gamesite.exception_log.event(server_time, 'destroy_object by %d: %s id %s killed by %s id %s owned by same team. Likely hacking attempt.' % (session.player.user_id, obj.spec.name, obj.obj_id, killer_obj.spec.name, killer_obj.obj_id))
+            gamesite.exception_log.event(server_time, 'destroy_object: player %d session had %s id %s killed by %s id %s both owned by %s. Possible hacking attempt.' % (session.player.user_id, obj.spec.name, obj.obj_id, killer_obj.spec.name, killer_obj.obj_id, str(obj.owner.user_id)))
 
         # only mobile units get destroyed permanently
         assert obj.is_mobile()
