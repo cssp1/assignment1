217fe96d0615a7ae64aafce46151e579c67c9791
James Davis <6515965+The-Davis@users.noreply.github.com>
2022-05-24 13:19:54 -0400

    Fix log file name

--- aws/prod/etc/cron.patreon/patreon-rewards.sh
+++ aws/prod/etc/cron.patreon/patreon-rewards.sh
@@ -9,7 +9,7 @@ cd $GAME_DIR/gameserver
 
 TODAY=`date +%Y%m%d`
 TIME=`date`
-LOG=logs/$TODAY-maint.txt
+LOG=logs/$TODAY-patron-rewards.txt
 
 echo "====== ${TIME} ======" >> $LOG
 ./SpinNoSQL.py --patron-rewards >> $LOG
